fd6dacc0f9df8ccc28a0370c5936daa8353c2b7c
Ken Cowles <krcowles29@gmail.com>
2016-08-08 14:57:12 -0600

    trying track-draw on index page map

--- scripts/map.js
+++ scripts/map.js
@@ -208,105 +208,105 @@ if ( useTbl ) {
 // HIKE DATA ARRAYS:
 // [ 'Name', lat, lng, html source for page, GPX file present (true/false) ]
 var ctrPinHikes = [
-	['Bandelier',35.779039,-106.270788,'Bandelier.html',0],
-	['Chaco Canyon',36.030250,-107.91080,'Chaco.html',0],
-	['El Malpais',34.970407,-107.810152,'ElMalpais.html',0],
-	['Petroglyphs Natl Mon',35.138644,-106.711196,'Petroglyphs.html',0]
+	['Bandelier',35.779039,-106.270788,'Bandelier.html',''],
+	['Chaco Canyon',36.030250,-107.91080,'Chaco.html',''],
+	['El Malpais',34.970407,-107.810152,'ElMalpais.html',''],
+	['Petroglyphs Natl Mon',35.138644,-106.711196,'Petroglyphs.html','']
 ];
 var clusterPinHikes = [
 	// Bandelier hikes:
-	['Ruins Trail',35.793670,-106.273155,'MainLoop.html',0],
-	['Falls Trail',35.788735,-106.282079,'FallsTrail.html',0],
-	['Frey Trail',35.779219,-106.285744,'Frey.html',0],
-	['Frijolito Ruins',35.769573,-106.282433,'Frijolito.html',0],
-	['Alcove House',35.764312,-106.273698,'AlcoveHouse.html',0],
-	['Tsankawi Ruins',35.860416,-106.224682,'Tsankawi.html',0],
+	['Ruins Trail',35.793670,-106.273155,'MainLoop.html',''],
+	['Falls Trail',35.788735,-106.282079,'FallsTrail.html',''],
+	['Frey Trail',35.779219,-106.285744,'Frey.html',''],
+	['Frijolito Ruins',35.769573,-106.282433,'Frijolito.html',''],
+	['Alcove House',35.764312,-106.273698,'AlcoveHouse.html',''],
+	['Tsankawi Ruins',35.860416,-106.224682,'Tsankawi.html',''],
 	// Bosque del Apache hikes:
-	['Canyon Trail',33.759012,-106.895278,'CanyonTrail.html',0],
+	['Canyon Trail',33.759012,-106.895278,'CanyonTrail.html',''],
 	// Chaco Canyon hikes:
-	['Una Vida',36.033331,-107.911942,'UnaVida.html',0],
-	['Hungo Pavi',36.049536,-107.93031,'HungoPavi.html',0],
-	['Pueblo Bonito',36.059216,-107.958934,'Bonito.html',0],
-	['Pueblo Alto',36.068608,-107.959900,'PuebloAlto.html',1],
-	['Kin Kletso',36.063864,-107.981315,'KinKletso.html',0],
+	['Una Vida',36.033331,-107.911942,'UnaVida.html',''],
+	['Hungo Pavi',36.049536,-107.93031,'HungoPavi.html',''],
+	['Pueblo Bonito',36.059216,-107.958934,'Bonito.html',''],
+	['Pueblo Alto',36.068608,-107.959900,'PuebloAlto.html','palto.json'],
+	['Kin Kletso',36.063864,-107.981315,'KinKletso.html',''],
 	// El Malpais hikes:
-	['Big Tubes',34.944733,-108.106983,'BigTubes.html',1],
-	['Ice Caves',34.99311,-108.080084,'IceCave.html',0],
-	['El Calderon',34.9698,-108.00325,'ElCalderon.html',1],
+	['Big Tubes',34.944733,-108.106983,'BigTubes.html','tubes.json'],
+	['Ice Caves',34.99311,-108.080084,'IceCave.html',''],
+	['El Calderon',34.9698,-108.00325,'ElCalderon.html','cald.json'],
 	// Elena Gallegos hikes:
-	['Pino Trail',35.160419, -106.463184,'Pino.html',1],
-	['Domingo Baca',35.167093,-106.465502,'Domingo.html',1],
+	['Pino Trail',35.160419, -106.463184,'Pino.html','pino.json'],
+	['Domingo Baca',35.167093,-106.465502,'Domingo.html','baca.json'],
 	// Ghost Ranch hikes:
-	['Chimney Rock',36.330525,-106.47482,'ChimneyRock.html',0],
-	['Kitchen Mesa',36.336353,-106.469007,'Kitchen.html',0],
+	['Chimney Rock',36.330525,-106.47482,'ChimneyRock.html',''],
+	['Kitchen Mesa',36.336353,-106.469007,'Kitchen.html',''],
 	// Manzanitas Trail hikes:
-	['Tunnel Canyon',35.055938,-106.371517,'TunnelCanyon.html',1],
-	['Birdhouse Ridge',35.055938,-106.388512,'Birdhouse.html',1],
+	['Tunnel Canyon',35.055938,-106.371517,'TunnelCanyon.html','tun.json'],
+	['Birdhouse Ridge',35.055938,-106.388512,'Birdhouse.html','bird.json'],
 	// Manzanos hikes:
-	['Albuquerque Trail',34.793491,-106.372268,'ABQ.html',0],
-	['July 4th Trail',34.790707,-106.382439,'July4.html',0],
+	['Albuquerque Trail',34.793491,-106.372268,'ABQ.html',''],
+	['July 4th Trail',34.790707,-106.382439,'July4.html',''],
 	// Petroglyphs hikes:
-	['Piedras Marcadas',35.188867,-106.686269,'Piedras.html',0],
-	['Mesa Point Trail',35.160629,-106.716645,'MesaPoint.html',0],
-	['Cliff Base Trail',35.165471,-106.729088,'CliffBase.html',0],
+	['Piedras Marcadas',35.188867,-106.686269,'Piedras.html',''],
+	['Mesa Point Trail',35.160629,-106.716645,'MesaPoint.html',''],
+	['Cliff Base Trail',35.165471,-106.729088,'CliffBase.html',''],
 	['Macaw Trail',35.170242,-106.717243,'Macaw.html',0],
-	['Rinconada Canyon',35.126851,-106.724635,'Rinconada.html',0],
-	['ABQ Volcanoes',35.13075,-106.7802667,'ABQVolcanoes.html',1],
+	['Rinconada Canyon',35.126851,-106.724635,'Rinconada.html',''],
+	['ABQ Volcanoes',35.13075,-106.7802667,'ABQVolcanoes.html','volc.json'],
 	// Big Tesuque Campground hikes:
-	['Upper Tesuque',35.764427,-105.769501,'UpperTesuque.html',1],
-	['Middle Tesuque',35.738236,-105.779114,'MiddleTesuque.html',1],
+	['Upper Tesuque',35.764427,-105.769501,'UpperTesuque.html','utes.json'],
+	['Middle Tesuque',35.738236,-105.779114,'MiddleTesuque.html','mtes.json'],
 	// Winsor Trailhead hikes:
-	['Deception Pk',35.807036,-105.783577,'Deception.html',1],
-	['Nambe Lake',35.818627,-105.797649,'Nambe.html',1],
-	['La Vega',35.816873,-105.815796,'LaVega.html',1],
-	['Upper Rio En Medio',35.802801,-105.827387,'UpperRio.html',1]
+	['Deception Pk',35.807036,-105.783577,'Deception.html','decp.json'],
+	['Nambe Lake',35.818627,-105.797649,'Nambe.html','nambe.json'],
+	['La Vega',35.816873,-105.815796,'LaVega.html','vega.json'],
+	['Upper Rio En Medio',35.802801,-105.827387,'UpperRio.html','uriom.json']
 ];
 var othrHikes = [
-	['Three Rivers',33.419574,-105.987682,'ThreeRivers.html',0],
-	['Corrales Acequia',35.249327,-106.607283,'Acequia.html',1],
-	['Agua Sarca',35.291533,-106.441050,'AguaSarca.html',1],
-	['Ancho Rapids',35.797000,-106.246417,'AnchoComb.html',1],
-	['Apache Canyon',35.629817,-105.858967,'ApacheCanyon.html',1],
-	['Aspen Vista',35.777433,-105.810933,'Aspen.html',1],
-	['Atalaya Mtn',35.670450,-105.900667,'Atalaya.html',1],
-	['Battleship Rock',35.828099,-106.641862,'Battleship.html',0],
-	['Borrego/Bear Wallow',35.7462,-105.8342667,'Borrego.html',1],
-	['Buckman Mesa',35.835833,-106.161033,'Buckman.html',1],
-	['Cabezon Pk',35.597,-107.1053833,'Cabezon.html',1],
-	['Cerrillos Hills',35.444819,-106.122029,'Cerrillos.html',0],
-	['Chamisa Trail',35.728417,-105.86597,'Chamisa.html',1],
-	['Chavez Canyon',36.367385,-106.677235,'ChavezCanyon.html',0],
-	['Coyote Call',35.848167,-106.465383,'CoyoteCall.html',1],
-	['Dale Ball North',35.71075,-105.899467,'DBallNorth.html',1],
-	['Del Agua',35.277,-106.4840333,'DelAguaHike.html',1],
-	['Diablo Canyon',35.8046,-106.1362333,'DiabloComb.html',1],
-	['El Morro',35.038224,-108.348783,'ElMorro.html',0],
-	['Ft Bayard Tree',32.782028,-108.147333,'FtBayard.html',0],
-	['Hyde Park Circle',35.730717,-105.8371,'HydePk.html',1],
-	['Josephs Mine',36.305933,-106.05142,'OjoCaliente.html',0],
-	['La Bajada',35.551633,-106.23655,'LaBajada.html',1],
-	['La Luz',35.219667,-106.4810167,'LaLuz.html',1],
-	['La Vista Verde',36.341432,-105.736461,'VistaVerde.html',0],
-	['Las Conchas Trail',35.814841,-106.533158,'Conchas.html',1],
-	['Mesa Chijuilla',35.995233,-107.0827,'Chijuilla.html',0],
-	['Mesa de Cuba',36.010603,-106.980625,'MesaCuba.html',0],
-	['Nature Conservancy',35.68701,-105.89697,'Conservancy.html',0],
-	['Ojito Wilderness',35.495067,-106.921767,'Ojito.html',1],
-	['Pinabete Tank',35.771583,-106.19055,'Pinabete.html',1],
-	['Purgatory Chasm',33.032667,-108.1536667,'Purgatory.html',1],
-	['Pyramid Rock',35.542743,-108.613801,'PyramidRock.html',0],
-	['Red Dot - Blue Dot',35.809767,-106.200917,'RedBlueComb.html',1],
-	['San Lorenzo Canyon',34.239571,-107.026899,'SanLorenzo.html',0],
-	['Strip Mine Trail',35.30015,-106.4804667,'StripMine.html',1],
-	['Sun Mountain',35.65675,-105.92095,'SunMountain.html',1],
-	['Tent Rocks',35.661033,-106.416106,'TentRocks.html',0],
-	['Tesuque-Lower',35.759783,-105.845917,'LowerTesuque.html',1],
-	['Catwalks',33.37781,-108.839842,'Catwalks.html',0],
-	['Tetilla Peak',35.602683,-106.19663,'Tetilla.html',1],
-	['Valle Grande',35.857077,-106.491058,'ValleGrandeInSnow.html',1],
-	['Viewpoint Loop',35.264798,-105.33362,'Villanueva.html',0],
-	['Williams Lake',36.572704,-105.436408,'WilliamsLake.html',0],
-	['Traders Trail',36.323333,-105.70366666,'Traders.html',1]
+	['Three Rivers',33.419574,-105.987682,'ThreeRivers.html',''],
+	['Corrales Acequia',35.249327,-106.607283,'Acequia.html',''], //
+	['Agua Sarca',35.291533,-106.441050,'AguaSarca.html',''], //
+	['Ancho Rapids',35.797000,-106.246417,'AnchoComb.html','ancho.json'],
+	['Apache Canyon',35.629817,-105.858967,'ApacheCanyon.html',''], //
+	['Aspen Vista',35.777433,-105.810933,'Aspen.html',''], //
+	['Atalaya Mtn',35.670450,-105.900667,'Atalaya.html',''], //
+	['Battleship Rock',35.828099,-106.641862,'Battleship.html',''],
+	['Borrego/Bear Wallow',35.7462,-105.8342667,'Borrego.html',''], //
+	['Buckman Mesa',35.835833,-106.161033,'Buckman.html',''], //
+	['Cabezon Pk',35.597,-107.1053833,'Cabezon.html',''], //
+	['Cerrillos Hills',35.444819,-106.122029,'Cerrillos.html',''],
+	['Chamisa Trail',35.728417,-105.86597,'Chamisa.html',''], //
+	['Chavez Canyon',36.367385,-106.677235,'ChavezCanyon.html',''],
+	['Coyote Call',35.848167,-106.465383,'CoyoteCall.html',''], //
+	['Dale Ball North',35.71075,-105.899467,'DBallNorth.html',''], //
+	['Del Agua',35.277,-106.4840333,'DelAguaHike.html',''], //
+	['Diablo Canyon',35.8046,-106.1362333,'DiabloComb.html',''], //
+	['El Morro',35.038224,-108.348783,'ElMorro.html',''],
+	['Ft Bayard Tree',32.782028,-108.147333,'FtBayard.html',''],
+	['Hyde Park Circle',35.730717,-105.8371,'HydePk.html',''], //
+	['Josephs Mine',36.305933,-106.05142,'OjoCaliente.html',''],
+	['La Bajada',35.551633,-106.23655,'LaBajada.html',''], //
+	['La Luz',35.219667,-106.4810167,'LaLuz.html',''], //
+	['La Vista Verde',36.341432,-105.736461,'VistaVerde.html',''],
+	['Las Conchas Trail',35.814841,-106.533158,'Conchas.html',''], //
+	['Mesa Chijuilla',35.995233,-107.0827,'Chijuilla.html',''],
+	['Mesa de Cuba',36.010603,-106.980625,'MesaCuba.html',''],
+	['Nature Conservancy',35.68701,-105.89697,'Conservancy.html',''],
+	['Ojito Wilderness',35.495067,-106.921767,'Ojito.html',''], //
+	['Pinabete Tank',35.771583,-106.19055,'Pinabete.html',''], //
+	['Purgatory Chasm',33.032667,-108.1536667,'Purgatory.html',''], //
+	['Pyramid Rock',35.542743,-108.613801,'PyramidRock.html',''],
+	['Red Dot - Blue Dot',35.809767,-106.200917,'RedBlueComb.html',''], //
+	['San Lorenzo Canyon',34.239571,-107.026899,'SanLorenzo.html',''],
+	['Strip Mine Trail',35.30015,-106.4804667,'StripMine.html',''], //
+	['Sun Mountain',35.65675,-105.92095,'SunMountain.html',''], //
+	['Tent Rocks',35.661033,-106.416106,'TentRocks.html',''],
+	['Tesuque-Lower',35.759783,-105.845917,'LowerTesuque.html',''], //
+	['Catwalks',33.37781,-108.839842,'Catwalks.html',''],
+	['Tetilla Peak',35.602683,-106.19663,'Tetilla.html',''], //
+	['Valle Grande',35.857077,-106.491058,'ValleGrandeInSnow.html',''], //
+	['Viewpoint Loop',35.264798,-105.33362,'Villanueva.html',''],
+	['Williams Lake',36.572704,-105.436408,'WilliamsLake.html',''],
+	['Traders Trail',36.323333,-105.70366666,'Traders.html',''] //
 ];
 
 // icon defs: need prefix when calling from full map page
@@ -445,7 +445,7 @@ function initMap() {
 		AddHikeMarker(loc, sym, nme, hpg);
 	}
 
-/* Establish polylines for areas where trailhead has more than 1 hike */
+	// Establish polylines for areas where trailhead has more than 1 hike
 	// BANDELIER:
 	var BandCtr = {lat: 35.778943, lng: -106.270838 };	
 	var BandHikeMrkrLocs = [ 
@@ -545,7 +545,6 @@ function initMap() {
 		strokeWeight: 2
 	});
 	CliffMacLines.setMap(null);
-	
 	// MANZANITAS MTN TRAILS:
 	var mmt = {lat: 35.046562, lng: -106.383088 };
 	var bhse = {lat: 35.055938, lng: -106.388512 };
@@ -559,7 +558,8 @@ function initMap() {
 		strokeWeight: 2
 	});
 	mmtLines.setMap(null);
-	/* END OF POLYLINES CREATION */
+	// END OF POLYLINES CREATION
+	
 	// PAN AND ZOOM HANDLERS:
 	map.addListener('zoom_changed', function() {
 		var curZoom = map.getZoom();
@@ -670,14 +670,72 @@ function IdTableElements(boundsStr) {
 	}
 } // END: IdTableElements() FUNCTION
 
-// ////////////////////////////  HIKING TRACKS  ////////////////////////
+// /////////////////////////////////////////////////////////////////////
+// //////////////////////////  HIKING TRACKS  //////////////////////////
+// /////////////////////////////////////////////////////////////////////
+var xhr = new XMLHttpRequest();
+var trackFile;
+var newTrack;
+var trkPtsArray = [];
 var trackForm = setInterval(drawTracks,200);
+
+
+xhr.onload = function() {
+	if ( xhr.status === 200 ) {
+		newTrack = JSON.parse(xhr.responseText); // array of objects (track points)
+		//msg = '<p>First data: ' + newTrack.track[0].lat + ', ' + newTrack.track[0].lng + '</p>';
+		//$('#dbug').append(msg);
+		for (var i=0; i<newTrack.track.length; i++) {
+			trkPtsArray.push(newTrack.track[i]);
+		}
+		var testTrack = new google.maps.Polyline({
+				path: trkPtsArray,
+				geodesic: true,
+				strokeColor: '#FF0000',
+				strokeOpacity: 1.0,
+				strokeWeight: 2
+			  });
+		testTrack.setMap(map);
+	} // end of successful load
+	
+	if ( xhr.status === 404 ) {
+		outTxt = '<p>URL NOT FOUND (ye olde message)</p>';
+		$('#dbug').append(outTxt);
+	}
+	
+	if (xhr.status === 500 ) {
+		outTxt = '<p>Some kind of internal server error...</p>';
+		$('#dbug').append(outTxt);
+	}
+}  // END OF 'onload' FUNCTION
+
+
+//JSONtrkFile = 'ancho.json';
+//xhr.overrideMimeType(); // trying to turn off "not well-formed" error-msg in Firefox
 
 function drawTracks() {
 	if ( mapRdy ) {
-		msg = '<p>Ready to draw tracks...</p>';
-		$('#dbug').append(msg);
 		clearInterval(trackForm);
+		// BIG LOOPS TO CATCH ALL THE HIKES w/GPX FILES...
+		// NO GPX files for Visitor Centers, so start with cluster hikes:
+		for ( var k=0; k<clusterPinHikes.length; k++ ) {
+			if ( clusterPinHikes[k][4] ) {
+				trackFile = clusterPinHikes[k][4];
+				msg = '<p>use ' + trackFile + '</p>';
+				$('#dbug').append(msg);
+				//xhr.open('GET',JSONtrkFile,true);
+				//xhr.send(null);
+			}
+		} // End of clusterPinHikes loop
+		for ( m=0; m<othrHikes.length; m++ ) {
+			if ( othrHikes[m][4] ) {
+				trackFile = othrHikes[m][4];
+				msg = '<p>othrHikes: use ' + trackFile + '</p>';
+				$('#dbug').append(msg);
+				xhr.open('GET','../test/ancho.json',true);
+				xhr.send(null);
+			}
+		}  // End of BIG LOOP
 	}
 }
 
