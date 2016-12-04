27f762bda48d4e511d4efb3401966c0158f7e8e1
Ken Cowles <krcowles29@gmail.com>
2016-12-04 08:58:56 -0700

    completed save form; eliminated security issue

--- build/modal_setup.js
+++ build/modal_setup.js
@@ -17,7 +17,7 @@ var modal = (function() {
 	/* FUNCTION TO CONVERT FORM DATA INTO STRING FOR SAVING IN LOCAL STORAGE */
 	function form2String(filledForm) {
 		formObject = new Object
-		filledForm.find("input, select").each(function() {
+		filledForm.find("input:not('#tsv'), select").each(function() {
 			if (this.id) {
 				elem = $(this);
 				if (elem.attr("type") == 'checkbox' || elem.attr("type") == 'radio') {
@@ -32,36 +32,54 @@ var modal = (function() {
 	}
 	/* FUNCTION TO CONVERT STRING BACK INTO FORM DATA */
 	function string2Form(formString, unfilledForm) {
-	formObject = JSON.parse(formString);
-	unfilledForm.find("input, select, textarea").each(function() {
-		if (this.id) {
-			id = this.id;
-			elem = $(this); 
-			if (elem.attr("type") == "checkbox" || elem.attr("type") == "radio" ) {
-				elem.attr("checked", formObject[id]);
-			} else {
-				elem.val(formObject[id]);
+		formObject = JSON.parse(formString);
+		unfilledForm.find("input:not('#tsv'), select, textarea").each(function() {
+			if (this.id) {
+				id = this.id;
+				elem = $(this); 
+				if (elem.attr("type") == "checkbox" || elem.attr("type") == "radio" ) {
+					elem.attr("checked", formObject[id]);
+				} else {
+					elem.val(formObject[id]);
+				}
 			}
-		}
-	});
-}
+		});
+	}
+	
 	// THE WORKHORSE TO SAVE THE FORM
 	function formSaver() {
 		var msg;
+		var prevName;
+		var curDiv;
 		var closeModal = true;
+		var ulfile = $('#ul').text();
+		if ( ulfile === '' ) {
+			ulfile = 'None';
+		}
 		var saveName = $('#savetxt').val();
 		if (saveName === '') {
 			window.alert("Please enter an identifier in text box!");
 			$(this).attr('checked',false);
 		} else {
-			// save the current state of the form
-			formString = form2String($('#hikeData'));
-			var currentSaves = parseFloat(window.localStorage.noOfSaves);
+			// there is a name to use for the save:
 			if (currentSaves >= 2) {
 				msg = 'ALREADY SAVED 2: Nothing new will be saved';
 				window.alert(msg);
-			} else if (currentSaves === 1) {
-				var prevName = window.localStorage.oldName1
+				modal.close();
+			} 
+			/* therefore, current no of saves is 0 or 1: */
+			// save the current state of the form
+			formString = form2String($('#hikeData'));
+			var currentSaves = parseFloat(window.localStorage.noOfSaves);
+			if (currentSaves === 1) {
+				// Already have one stored: must be EITHER LBL1 or LBL2
+				curDiv = $('#left1').html();
+				if (curDiv === '')	{
+					// left1 is empty, so the existing button is in left2
+					prevName = window.localStorage.oldName2;
+				} else { // existing button is in left1
+					prevName = window.localStorage.oldName1;
+				}
 				if (prevName == saveName) {
 					msg = 'Choose a different identifier - this was previously saved';
 					window.alert(msg);
@@ -69,53 +87,83 @@ var modal = (function() {
 					$('#closeit').prop('checked',false);
 					closeModal = false;
 				} else {
-					// SECOND ITEM STORED
-					window.localStorage.oldName2 = saveName;
-					window.localStorage.oldForm2 = formString;
-					window.localStorage.noOfSaves = 2;
-					msg = '<p id="second" style="margin-top:-1px;margin-bottom:0px;"><input type="radio" name="restores" value="name2" />' + saveName + '</p>';
-					$('#unsaver').after(msg);
-					$('#second').on('click', function() {
-						var restoredForm = window.localStorage.oldForm2;
-						string2Form(restoredForm, $('#theForm'));
-						currentSaves = parseFloat(window.localStorage.noOfSaves);
-						currentSaves -= 1;
-						if (currentSaves === 0) {
-							$('#unsaver').text('Restore a previously saved form: (currently none)');
-						}
-						window.localStorage.noOfSaves = currentSaves;
-						window.localStorage.removeItem('oldName2');
-						window.localStorage.removeItem('oldForm2');
-						$(this).remove();
-					});
-				}
-			} else {
-				// FIRST ITEM STORED:
+					if ( curDiv !== '' ) { 
+						// the left1 div is not empty, so create lbl2
+						window.localStorage.oldName2 = saveName;
+						window.localStorage.oldForm2 = formString;
+						window.localStorage.oldFile2 = ulfile;
+						window.localStorage.noOfSaves = 2;
+						msg = '<label id="lbl2"><input id="save2" type="radio" name="restores" /> ' + 
+							saveName + '; &nbsp;&nbsp;Uploaded file: ' + ulfile + '</label>';
+						$('#left2').append(msg);
+						msg = '<label id="lblr2"><input id="kill2" type="radio" name="delRestore" /> Remove ' +
+							saveName + '</label>';
+						$('#right2').append(msg);
+						// clear out the uploaded file name for use in other saves
+						$('#ul').text('');
+						$('#save2').on('click', function() {
+							var restoredForm = window.localStorage.oldForm2;
+							string2Form(restoredForm, $('#hikeData'));
+							$(this).attr('checked',false);
+						});
+						$('#kill2').on('click', function() {
+							var savesNow = parseFloat(window.localStorage.noOfSaves);
+							savesNow -= 1;
+							if (savesNow === 0) {
+								$('#unsaver').text('Restore a previously saved form: (currently none)');
+							}
+							window.localStorage.noOfSaves = savesNow;
+							window.localStorage.removeItem('oldName2');
+							window.localStorage.removeItem('oldForm2');
+							window.localStorage.removeItem('oldFile2');
+							$('#lbl2').remove();
+							$('#lblr2').remove();
+						});
+						modal.close();
+						return;
+					}  // end of lbl2 creation
+					// haven't done anything yet, lbl2 is the remaining button
+				}  // end of DUPLICATE NAME test					
+			} // end of no of saves = 1
+			if (closeModal === true) {
+				// Create lbl1: either no of saves = 0, or remaining saves is in left2
+				currentSaves++;  // either it moved from 0 to 1 if prev saves = 0, or
+								 // it moved from 1 to 2 if prev saves = 1
 				window.localStorage.oldName1 = saveName;
 				window.localStorage.oldForm1 = formString;
-				window.localStorage.noOfSaves = 1;
+				window.localStorage.oldFile1 = ulfile;
+				window.localStorage.noOfSaves = currentSaves;
 				$('#unsaver').text('Restore a previously saved form: ');
-				msg = '<p id="first" style="margin-top:-1px;"><input type="radio" name="restores" value="name1" />' + saveName + '</p>';
-				$('#unsaver').after(msg);
-				$('#first').on('click', function() {
-					var restoredForm = window.localStorage.oldForm1	;
-					string2Form(restoredForm, $('#theForm'));
-					currentSaves = parseFloat(window.localStorage.noOfSaves);
-					currentSaves -= 1;
-					if (currentSaves === 0) {
+				msg = '<label id="lbl1"><input id="save1" type="radio" name="restores" /> ' + 
+					saveName + '; &nbsp;&nbsp;Uploaded file: ' + ulfile + '</label>';
+				$('#left1').append(msg);
+				msg = '<label id="lblr1"><input id="kill1" type="radio" name="delRestore" /> Remove ' +
+					saveName + '</label>';
+				$('#right1').append(msg);
+				// clear out the uploaded file name for use in other saves
+				$('#ul').text('');
+				$('#save1').on('click', function() {
+					var restoredForm = window.localStorage.oldForm1;
+					string2Form(restoredForm, $('#hikeData'));
+					$(this).attr('checked',false);
+				});
+				$('#kill1').on('click', function() {
+					var currSaves = parseFloat(window.localStorage.noOfSaves);
+					currSaves -= 1;
+					if (currSaves === 0) {
 						$('#unsaver').text('Restore a previously saved form: (currently none)');
 					}
-					window.localStorage.noOfSaves = currentSaves;
+					window.localStorage.noOfSaves = currSaves;
 					window.localStorage.removeItem('oldName1');	
 					window.localStorage.removeItem('oldForm1');
-					$(this).remove();
+					window.localStorage.removeItem('oldFile1');
+					$('#lbl1').remove();
+					$('#lblr1').remove();
 				});
-			}
-			if (closeModal === true) {
 				modal.close();
-			}
-		}
-	}
+			}  // end of create lbl1 and close
+		}  // end of testing contents of 'saveName'
+	} // end of function formSaver()
 	
 	// public
 	return {   // returns object methods:
@@ -151,4 +199,4 @@ var modal = (function() {
 			$(window).off('resize', modal.center);
 		}
 	};
-}());  // IIFE
+}());  // modal is an IIFE
--- build/step1.js
+++ build/step1.js
@@ -1,14 +1,16 @@
 $( function () { // when page is loaded...
 
-if (typeof(Storage) != undefined) {
+if (typeof(Storage) !== undefined) {
 	var msg; //debug outputs
 	var previousSaves;
-	var buttonName, r1, r2;
+	var uploadedFile = '';
+	var buttonName;
+	var ulName;
 	
 	// FUNCTION FOR RESTORING FORM DATA FROM A STRING
 	function stringToForm(formString, unfilledForm) {
 		formObject = JSON.parse(formString);
-		unfilledForm.find("input, select, textarea").each(function() {
+		unfilledForm.find("input:not('#tsv'), select, textarea").each(function() {
 			if (this.id) {
 				id = this.id;
 				elem = $(this); 
@@ -21,29 +23,48 @@ if (typeof(Storage) != undefined) {
 		});
 	}
 
+	$('#tsv').change(function() {
+		var fullPath = document.getElementById('tsv').value;
+		if (fullPath) {
+			var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+			var uploadedFile = fullPath.substring(startIndex);
+			if (uploadedFile.indexOf('\\') === 0 || uploadedFile.indexOf('/') === 0) {
+				uploadedFile = uploadedFile.substring(1);
+			}
+		}
+		$('#ul').append(uploadedFile);
+		//window.alert(uploadedFile);
+	});
+	
 	// MODAL WINDOW SETUP AND EVENT DEFINITION
 	var $savePopup = $('#save-modal').detach();
 	$('#saver').on('click', function() {
 		modal.open({id: 'saver', content: $savePopup, width:400, height:200});
+		/* NOTE: "id" key added as it was thought there would be other modal windows to process,
+		   and the routine (modal_setup.js) would need to know which type modal to produce */
 	});
 	// DEBUG STUFF...
 	$('#dbugr').on('click', function() {
 		var a = window.localStorage.noOfSaves;
 		var b = window.localStorage.oldName1;
 		var c = window.localStorage.oldForm1;
-		var d = window.localStorage.oldName2;
-		var e = window.localStorage.oldForm2;
+		var d = window.localStorage.oldFile1;
+		var e = window.localStorage.oldName2;
+		var f = window.localStorage.oldForm2;
+		var g = window.localStorage.oldList2;
 		var dout = '<p>Current storage data:</p> ';
-		dout += '<p>Saves: ' + a + '</p><p> Item1: ' + b + ' / ' + c + '</p>';
-		dout += '<p>Item2: ' + d + ' / ' + e + '</p>';
+		dout += '<p>Saves: ' + a + '</p><p> Item1: ' + b + ' / ' + d + ';   ' + c + '</p>';
+		dout += '<p>Item2: ' + e + ' / ' + g + '; ' + f + '</p>';
 		$(this).prepend(dout);
 	});
 	$('#cleaner').on('click', function() {
 		window.localStorage.noOfSaves = 0;
 		window.localStorage.removeItem('oldName1');
 		window.localStorage.removeItem('oldForm1');
+		window.localStorage.removeItem('oldFile1');
 		window.localStorage.removeItem('oldName2');
 		window.localStorage.removeItem('oldForm2');
+		window.localStorage.removeITem('oldFile2');
 	});
 
 	// Load previous saves and set event handlers
@@ -51,38 +72,62 @@ if (typeof(Storage) != undefined) {
 	if (previousSaves > 0) {
 		$('#unsaver').text('Restore a previously saved form:');
 		buttonName = window.localStorage.oldName1;
-		r1 = '<p id="restore1" style="margin-top:3px;margin-bottom:0px;"><input type="radio" name="restores" value="r1" />' + buttonName +
-			' [oldest]<br /></p>';
-		$('#unsaver').append(r1);
-		$('#restore1').on('click', function() {
+		ulName = window.localStorage.oldFile1;
+		msg = '<label id="lbl1"><input id="save1" type="radio" name="restores" /> ' + 
+				buttonName + '; &nbsp;&nbsp;Uploaded file: ' + ulName + '</label>';
+		$('#left1').append(msg);
+		msg = '<label id="lblr1"><input id="kill1" type="radio" name="delRestore" /> Remove ' +
+				buttonName + '</label>';
+		$('#right1').append(msg);
+		// ensure ul filename space is empty to start with
+		$('#ul').text('');
+		$('#save1').on('click', function() {
 			var restoredForm = window.localStorage.oldForm1;
-			stringToForm(restoredForm, $('#theForm'));
-			previousSaves -= 1;
-			if (previousSaves === 0) {
+			stringToForm(restoredForm, $('#hikeData'));
+			$(this).attr('checked',false);
+		});
+		$('#kill1').on('click', function() {
+			var currSaves = parseFloat(window.localStorage.noOfSaves);
+			currSaves -= 1;
+			if (currSaves === 0) {
 				$('#unsaver').text('Restore a previously saved form: (currently none)');
 			}
-			window.localStorage.noOfSaves = previousSaves;
+			window.localStorage.noOfSaves = currSaves;
 			window.localStorage.removeItem('oldName1');	
 			window.localStorage.removeItem('oldForm1');
-			$(this).remove();
+			window.localStorage.removeItem('oldFile1');
+			$('#lbl1').remove();
+			$('#lblr1').remove();
 		});
 	}
 	if (previousSaves > 1) {
 		buttonName = window.localStorage.oldName2;
-		r2 = '<p id="restore2" style="margin-top:-1px;"><input id="restore2" type="radio" name="restores" value="r2" />' +
-			buttonName + '</p>';
-		$('#unsaver').append(r2);
-		$('#restore2').on('click', function() {
+		ulName = window.localStorage.oldName2;
+		msg = '<label id="lbl2"><input id="save2" type="radio" name="restores" /> ' + 
+			buttonName + '; &nbsp;&nbsp;Uploaded file: ' + ulName + '</label>';
+		$('#left2').append(msg);
+		msg = '<label id="lblr2"><input id="kill2" type="radio" name="delRestore" /> Remove ' +
+			buttonName + '</label>';
+		$('#right2').append(msg);
+		// clear out the uploaded file name for use in other saves
+		$('#ul').text('');
+		$('#save2').on('click', function() {
 			var restoredForm = window.localStorage.oldForm2;
-			stringToForm(restoredForm, $('#theForm'));
-			previousSaves -= 1;
-			if (previousSaves === 0) {
+			stringToForm(restoredForm, $('#hikeData'));
+			$(this).attr('checked',false);
+		});
+		$('#kill2').on('click', function() {
+			var savesNow = parseFloat(window.localStorage.noOfSaves);
+			savesNow -= 1;
+			if (savesNow === 0) {
 				$('#unsaver').text('Restore a previously saved form: (currently none)');
 			}
-			window.localStorage.noOfSaves = previousSaves;
+			window.localStorage.noOfSaves = savesNow;
 			window.localStorage.removeItem('oldName2');
 			window.localStorage.removeItem('oldForm2');
-			$(this).remove();
+			window.localStorage.removeItem('oldFile2');
+			$('#lbl2').remove();
+			$('#lblr2').remove();
 		});
 	}
 } else {
