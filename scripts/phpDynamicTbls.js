e10b6d2febb03413ec3efdac3bf1eb0f13bbc331
Ken Cowles <krcowles29@gmail.com>
2016-12-26 09:45:46 -0700

    typo in DeNaZin track file; fixed jQuery selection for rows in phpDynamicTbles.js

--- scripts/phpDynamicTbls.js
+++ scripts/phpDynamicTbls.js
@@ -268,53 +268,21 @@ function IdTableElements(boundsStr) {
 	// REMOVE previous table:
 	$('div #usrTbl').replaceWith('<div id="usrTbl"></div>');
 	/* FIND HIKES WITHIN THE CURRENT VIEWPORT BOUNDS */
-	// First, check to see if any ctrPinHikes are within the viewport;
-	// if so, include them in the table
 	var n = 0;
 	var rowCnt = 0;
-	$('table tbody tr').each( function() {
-		pinLat = parseFloat($(this).data('lat'));
-		pinLon = parseFloat($(this).data('lon'));	
+	// back to this anomalous behavior where jQuery returns  header row with trs in 'tbody'
+	var $allRows = $('#refTbl tbody tr');
+	for (i=1; i<$allRows.length; i++) {  // note that i starts at 1 instead of 0
+		var tmpLat = $($allRows[i]).data('lat');
+		var tmpLng = $($allRows[i]).data('lon');
+		pinLat = parseFloat(tmpLat);
+		pinLon = parseFloat(tmpLng);	
 		if( pinLon <= east && pinLon >= west && pinLat <= north && pinLat >= south ) {
-			tblEl[n] = j;
+			tblEl[n] = i-1;
 			n++;
 			rowCnt ++;
 		}	
-	});
-	/*
-	for (j=0; j<ctrPinHikes.length; j++) {
-		hikeSet = ctrPinHikes[j];
-		pinLat = parseFloat(hikeSet[1]);
-		pinLng = parseFloat(hikeSet[2]);
-		if( pinLng <= east && pinLng >= west && pinLat <= north && pinLat >= south ) {
-			tblEl[n] = j;
-			n++;
-			rowCnt ++;
-		}
 	}
-	// now look for clusterPinHikes
-	for (k=0; k<clusterPinHikes.length; k++) {
-		hikeSet = clusterPinHikes[k];
-		pinLat = parseFloat(hikeSet[1]);
-		pinLng = parseFloat(hikeSet[2]);
-		if( pinLng <= east && pinLng >= west && pinLat <= north && pinLat >= south ) {
-			tblEl[n] = ctrPinHikes.length + k;
-			n++;
-			rowCnt++;
-		}		
-	}
-	// and lastly, othrHikes
-	for (l=0; l<othrHikes.length; l++) {
-		hikeSet = othrHikes[l];
-		pinLat = parseFloat(hikeSet[1]);
-		pinLng = parseFloat(hikeSet[2]);
-		if( pinLng <= east && pinLng >= west && pinLat <= north && pinLat >= south ) {
-			tblEl[n] = ctrPinHikes.length + clusterPinHikes.length + l;
-			n++;
-			rowCnt++;
-		}
-	}
-	*/
 	if ( rowCnt === 0 ) {
 		msg = '<p>NO hikes in this area</p>';;
 		$('#usrTbl').append(msg);
