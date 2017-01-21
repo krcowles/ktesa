cf145f17a07f65a40206176fd37eb67aa9cd9cfb
Ken Cowles <krcowles29@gmail.com>
2017-01-21 15:32:09 -0700

    added expert options with reistration key

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,32 @@
 $( function() {  // wait until document is loaded...
 // ------------------
 
+
+
+$('#auxfrm').submit( function() {
+	var keyval = this.regkey.value;
+	if (keyval == '1948') {
+		$('#experts').css('display','block');
+		$('#editor').on('click', function() {
+			var editUrl = 'build/hikeEditor.php';
+			window.open(editUrl);
+		});
+		$('#creator').on('click', function() {
+			var createUrl = 'build/enterHike.html';
+			window.open(createUrl);
+		});
+		$('#hide').on('click', function() {
+			$("input[type='password']").val('');
+			$('#experts').css('display','none');
+		});
+	} else {
+		window.alert("Key not recognized");
+	}
+	return false;
+});
+// IF KEY SUCCESSFUL:
+
+
 $('#turnon').on('click', function() {
 	$('#more').css('display','block');
 	$(this).css('display','none');
