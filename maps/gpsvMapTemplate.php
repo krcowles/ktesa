596dc8a63343a753d8b3e6701d108b461136ea7a
tjsandberg <tjsandberg@yahoo.com>
2017-02-05 08:52:35 -0700

    Add two more GPSV map url parameters: capability to suppress markers and street view.

--- maps/gpsvMapTemplate.php
+++ maps/gpsvMapTemplate.php
@@ -1,7 +1,13 @@
 <?php
 	$lines = file($_GET[map_name]);
 	for($i = 0; $i < count($lines); ++$i) {
-		echo ($lines[$i]);
+		// if (strpos($lines[$i], "GV_Draw_Marker") === false || ($_GET[show_markers_url] === true)) { // suppress markers? 
+		if (strpos($lines[$i], "GV_Draw_Marker") === false) {  
+			echo ($lines[$i]);
+		}
+		elseif ($_GET[show_markers_url] == true) {
+			echo ($lines[$i]);		// suppress markers per url param
+		}
 		if (strpos($lines[$i], "Although GPS Visualizer didn't create") !== false) {
 			include 'map_geoloc.php';		// insert geolocation code
 		}
