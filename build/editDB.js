14df816e737f3cfa4cb48059aaab96b5df1ca97c
Ken Cowles <krcowles29@gmail.com>
2017-03-11 15:02:08 -0700

    implemented state machine behavior for cluster assignments, but not in save yet

--- build/editDB.js
+++ build/editDB.js
@@ -1,12 +1,12 @@
 $( function () { // when page is loaded...
-
+	
 /* Each drop-down field parameter is held in a hidden <p> element; the data (text)
    in that hidden <p> element is the default that should appear in the drop-down box
    The drop-down style parameters are:
    		- locale
    		- cluster group name
    		- hike type
-   		- difficulty
+   		- difficult
    		- exposure
    		- references
 */
@@ -14,33 +14,108 @@ $( function () { // when page is loaded...
 // Locale:
 var sel = $('#locality').text();
 $('#area').val(sel);
-// Cluster group info:
-var clusnme = $('#group').text();  // previous cluster for hike being edited (may be empty)
-var currnme = clusnme;  // current selection value
-$('#ctip').val(clusnme);  // show above in select box on page load
-var mrkr = $('#mrkr').text();  // original state of marker type
-var msg = "&nbsp;&nbsp;&nbsp;Restore Marker to: " + mrkr + "&nbsp;&nbsp;";
-$('#chgBack').prepend(msg);  // if cluster is selected, record previous state
 
-/* Special case: when changing to or from a cluster marker */
-var warned = false;
-$('#ctip').change(function() {  // record any changes to the marker type
-	// even if changed back later, record change:
-	$('#grpChg').val("YES");
-	/* If marker was not originally a cluster type, prepare to change: */
-	if (mrkr !== 'Cluster') {
-		if (!warned) {
-			window.alert("Marker will be changed to cluster type");
-			$('#addbrk').css('display','block');
-			$('#chg2Clus').val("YES");
-			// display option to restore marker type:
-			$('#oldmrkr').val(mrkr);
-			$('#chgBack').css('display','inline');
-			warned = true;
+/* 
+ * THE FOLLOWING CODE ADDRESSES EDITS TO CLUSTER ASSIGNMENTS
+ */
+var msg;
+var rule = "The specified new group will be added;" + "\n" + 
+	"Current Cluster assignment will be ignored" + "\n" + 
+	"Uncheck box to use currently available groups";
+var clusnme = $('#group').text();  // incoming cluster assignment for hike being edited (may be empty)
+$('#ctip').val(clusnme);  // show above in the select box on page load
+if (clusnme == '') {  // no incoming assignment; marker is not cluster type; display info:
+	$('#notclus').css('display','inline');
+} else {
+	$('#showdel').css('display','block');
+}
+var mrkr = $('#mrkr').text();
+/* To correctly process changes involving cluster types, the following state information
+   needs to be passed to the server:
+   		1. Restore original assignments?  ignore
+   		2. Is a totally new cluster group being assigned?  (#newg) nxtg
+   		   (Whether or not the previous type was cluster, new info will be extracted at server,
+   		    unless "ignore" is checked to restore original state)
+   		3. Was marker changed from a non-cluster type to a cluster?  (#mrkrchg) chg2clus
+   		4. Was a group different from the original group selected in the drop-down #ctip?  (#grpchg) chgd
+   		5. Remove an existing cluster assignment?  (#deassign) rmclus
+*/
+var fieldflag = false;  // validation: make sure newt gets entered when newg is checked
+// RESTORE INCOMING DEFAULTS:
+$('#ignore').change(function() {
+	if (this.checked) {
+		$('#ctip').val(clusnme);
+		if (clusnme == '') {
+			$('#notclus').css('display','inline');
 		}
+		$('input:checkbox[name=nxtg]').attr('checked',false);
+		$('#newg').val("NO");
+		$('#newt').val("");
+		$('#mrkrchg').val("NO");
+		$('#grpchg').val("NO");
+		$('#deassign').val("NO");
+		$('input:checkbox[name=rmclus]').attr('checked',false);
+		fieldflag = false;
+		window.alert("Original state restored" + "\n" + "No edits at this time to clusters");
+		this.checked = false;
 	}
 });
-	
+// TELL SERVER TO REMOVE THE CLUSTER ASSIGNMENT AND CHANGE MARKER TO NORMAL
+$('#deassign').change(function() {
+	if (this.checked) {
+		$('#deassign').val("YES");
+	} else {
+		$('#deassign').val("NO");
+	}
+});
+// ASSIGN BRAND NEW GROUP:
+$('#newg').change(function() {
+	if (this.checked) {
+		this.value = "YES";
+		if (clusnme == '') {
+			$('#notclus').css('display','none');
+			$('#mrkrchg').val("YES");	
+		}
+		fieldflag = true;
+		$('#grpchg').val("NO");
+		window.alert(rule);
+	} else {  // newg is unchecked
+		this.value = "NO";
+		if (clusnme == '') {
+			$('#notclus').css('display','inline');
+			$('#mrkrchg').val("NO");
+			$('#ctip').val(clusnme);
+		}
+		$('#newt').val("");
+		fieldflag = false;				
+	}
+	$('#ctip').val(clusnme); // go back to original group assignment
+});
+// GROUP ASSIGNMENT DROP_DOWN VALUE IS CHANGED:
+$('#ctip').change(function() {  // record any changes to the cluster assignment
+	if ( $('#newg').val() === 'NO' ) {
+		if (this.value !== clusnme) {
+			/* If marker was not originally a cluster type, prepare to change to cluster group: */
+			if (mrkr !== 'Cluster') {
+				window.alert("Marker will be changed to cluster type");
+				$('#mrkrchg').val("YES");
+				$('#notclus').css('display','none');
+			} else {  // otherwise, let user know the existing cluster group assignment will change
+				msg = "Cluster type will be changed from " + "\n" + "Original setting of: " + clusnme + "\n";
+				msg += "To: " + $('#ctip').val();
+				window.alert(msg);
+			}
+			$('#grpchg').val("YES");
+		} 
+	} else {  // when/if the box gets unchecked, the ctip val will be restored to original state;
+		// deactivate grpchg to align with restored original cluster assignment
+		$('#grpchg').val("NO");
+		window.alert("Changes ignored while New Group Box is checked");
+	}
+});
+
+
+
 // Hike type:
 var htype = $('#ctype').text();
 $('#type').val(htype);
