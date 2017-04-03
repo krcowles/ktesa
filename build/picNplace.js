ed0a8292b5fc0018904fc43cd214b1a9db3b9c8d
Ken Cowles <krcowles29@gmail.com>
2017-04-03 08:00:05 -0600

    additional scaling for maps

--- build/picNplace.js
+++ build/picNplace.js
@@ -1,4 +1,10 @@
 // This routine is called early - before html - in order to set up drag/drop functions
+
+// the following are established to echo the constants used in editDB.php
+var alpha = 30;
+var beta = 10;
+var insertDelta = beta - alpha;  // to be added to image width to determine margin-left
+// globals to capture detached images and info about drag/drop rows
 var dragRow;
 var draggedImg;  // pic, noncap pic, or iframe
 var draggedInsert;
@@ -80,6 +86,7 @@ function increaseImgCnt(targ) {
 	var dropCapDiv;
 	var dropCapChild;
 	var currWidth = 30;   // current design: width of symbol used to indicate insertion points
+	var diWd;
 	
 	// get the row number for the drop from insertRow:
 	var childId = '#' + targetInsert;
@@ -89,18 +96,50 @@ function increaseImgCnt(targ) {
 	// corresponding image row id:
 	rowId = '#row' + dropRow;
 	var $dropChildren = $(rowId).children();
-	// scale this image to the current rowht:
+	// scale the dragged image to the current rowht:
 	var currRowHt = $dropChildren.eq(0).height();
-	var diHt = draggedImg[0].height;
-	var diScale = currRowHt/diHt;
-	diWd = Math.floor(diScale * draggedImg[0].width);
-	draggedImg[0].height = currRowHt;
-	draggedImg[0].width = diWd;
-	// also scale the inserts and captions:
-	var insMarg = draggedInsert.css('margin-left');
-	insMarg = parseInt(insMarg.replace('px',''));
-	insMarg = Math.floor(diScale * insMarg);
-	draggedInsert.css('margin-left','insMarg');
+	if (draggedImg[0].id === 'map0') {
+		var mapNode = draggedImg[0].firstChild;
+		var diHt = mapNode.height;
+		var diScale = currRowHt/diHt;
+		diWd = Math.floor(diScale * diHt);  // height & width are equal for map
+		mapNode.height = diWd;
+		mapNode.width = diWd;	
+	} else {
+		var diHt = draggedImg[0].height;
+		var diScale = currRowHt/diHt;
+		diWd = Math.floor(diScale * draggedImg[0].width);
+		draggedImg[0].height = currRowHt;
+		draggedImg[0].width = diWd;
+	}
+	/* the new left-marg must equal the <<width of the preceding image>> + insertDelta
+	 * IF insert is at 'lead', there is no calculation required beyond scaling
+	 * IF insert is at 'insN', find that node numer and decrement by 1 
+	 */
+	var insertAtLead = false;
+	if (targetInsert.substring(0,3) === 'lea') { insertAtLead = true; }
+	var insMarg;
+	if (insertAtLead) {
+		// scale to new row
+		insMarg = draggedInsert.css('margin-left');
+		insMarg = parseInt(insMarg.replace('px',''));
+		insMarg = Math.floor(diScale * insMarg) + 'px'
+	} else { 
+		// find node number of image to use:
+		dropInsParent = document.getElementById(insParentRowId);
+		var targChildren = dropInsParent.childNodes;
+		var childNodeNo = 0;
+		for (var i=1; i<targChildren.length; i++) {  // skip over 'lead' insert
+			if(targChildren[i].id === targetInsert) {  
+				childNodeNo = i;  
+				break;
+			}
+		}
+		var margImg = i - 1;
+		insMarg = ($dropChildren.eq(margImg).width() + insertDelta) + 'px';    // MAP?!!!
+	} 
+	draggedInsert.css('margin-left',insMarg);
+	// also scale the captions:
 	var capWidth = draggedCap.width();
 	capWidth = Math.floor(diScale * capWidth) + 10;
 	draggedCap.width(capWidth);
@@ -122,8 +161,6 @@ function increaseImgCnt(targ) {
 	 * IF SO, call 'fitToNewRow()'
 	 */
 	if (insWidth > maxRow) { fitToNewRow(dropRow, insWidth); }
-	var insertAtLead = false;
-	if (targetInsert.substring(0,3) === 'lea') { insertAtLead = true; }
 	/* 
 	 * The drop row is extracted from either the 'lead' insert (followed by row no)
 	 * or, the insert number (get parent row's attribute id)
@@ -147,15 +184,6 @@ function increaseImgCnt(targ) {
 		dropCapDiv.insertBefore(draggedCap[0],dropCapChild);
 	} else { 
 		// which 'insert' child is the target in the node list? (will correspond to row el)
-		dropInsParent = document.getElementById(insParentRowId);
-		var targChildren = dropInsParent.childNodes;
-		var childNodeNo = 0;
-		for (var i=1; i<targChildren.length; i++) {  // skip over 'lead' insert
-			if(targChildren[i].id === targetInsert) {  
-				childNodeNo = i;  
-				break;
-			}
-		}
 		// insert image:
 		var rowChildren = dropParentNode.childNodes;
 		dropChildNode = rowChildren[childNodeNo];
@@ -178,33 +206,43 @@ function fitToNewRow(rowToFit, triggerWidth) {
 	var oldHt = $items.eq(0).attr('height');
 	var newHt = Math.floor(scale * oldHt);
 	// ------ fit images:
-	var tstDiv;
+	var rowWidths = [];
+	var imgEl;
 	$items.each( function() {
-		tstDiv = this.id;
-		tstDiv = tstDiv.substring(0,3);
-		if (tstDiv === 'map') { 
+		imgEl = this.id;
+		imgEl = imgEl.substring(0,3);
+		if (imgEl === 'map') { 
 			var ifrm = document.getElementById('theMap');
 			var mapWd = ifrm.width;
 			mapWd = Math.floor(scale * mapWd);
 			ifrm.width = mapWd;
 			ifrm.height = newHt;
+			rowWidths.push(mapWd);
 		} else {
-			this.width = Math.floor(scale * this.width);
+			var newWd = Math.floor(scale * this.width);
+			this.width = newWd;
 			this.height = newHt;
+			rowWidths.push(newWd);
 		}
 	});
-	draggedImg[0].width = Math.floor(scale * draggedImg[0].width);
-	draggedImg[0].height = newHt;
+	if (draggedImg[0].id === 'map0') {
+		var mapNode = draggedImg[0].firstChild;
+		var mapDims = Math.floor(scale * mapNode.height);
+		mapNode.height = mapDims;
+		mapNode.width = mapDims;
+	} else {
+		draggedImg[0].width = Math.floor(scale * draggedImg[0].width);
+		draggedImg[0].height = newHt;
+	}
 	// ------ fit inserts:
 	var newMarg;
 	var insRow = '#insRow' + rowToFit;
 	var $inserts = $(insRow).children();
 	var skipLead = false;
+	var node = 0;
 	$inserts.each( function() {
 		if (skipLead) {
-			newMarg = $(this).css('margin-left');
-			newMarg = parseInt(newMarg.replace('px',''));
-			newMarg = Math.floor(scale * newMarg);
+			newMarg = (rowWidths[node] + insertDelta) + 'px';
 			$(this).css('margin-left', newMarg);
 		} else {
 			skipLead = true;
@@ -212,10 +250,10 @@ function fitToNewRow(rowToFit, triggerWidth) {
 	});
 	newMarg = draggedInsert.css('margin-left');
 	newMarg = parseInt(newMarg.replace('px',''));
-	newMarg = Math.floor(scale * newMarg) - 10  // 10 for inter-image space
+	newMarg = Math.floor(scale * newMarg) + 'px';
 	draggedInsert.css('margin-left',newMarg);
 	// ------ fit caption areas:
-	var caprow = '#caps' + rowToFit
+	var caprow = '#caps' + rowToFit;
 	var $capFields = $(caprow).children();
 	$capFields.each( function() {
 		if ( $(this).hasClass('notTA') ) {
