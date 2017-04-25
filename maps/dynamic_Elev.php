2194302d42df8c09b0e197a2de7b5697574af7a6
tjsandberg <tjsandberg@yahoo.com>
2017-04-25 17:24:45 -0600

    Try to get GV_Marker to work instead of google cricle.

--- maps/dynamic_Elev.php
+++ maps/dynamic_Elev.php
@@ -1,13 +1,7 @@
 	function drawCircle( circCtr ) {
-		chartMrkr = new google.maps.Circle({
-			strokeColor: '#FF0000',
-			strokeOpacity: 0.8,
-			strokeWeight: 3,
-			fillColor: '#FF0000',
-			fillOpacity: 0.35,
-			center: circCtr,
-			map: gmap,
-			radius: 40
+		chartMrkr = GV_Draw_Marker({
+			lat: circCtr.lat,
+			lon: circCtr.lon
 		});
 		circSet = true;
 	}
