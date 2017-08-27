8168bca2eb2182ceb6d8202950e8bfd4a814de59
Ken Cowles <krcowles29@gmail.com>
2017-08-27 14:41:37 -0600

    added table: 'tst'

--- php/000mysql_exe.php
+++ php/000mysql_exe.php
@@ -13,17 +13,24 @@
     <div>
 <?php
     require '000mysql_connect.php';
-    
+    echo "Opened";
     /* Depending on what type of query is made, php will need to respond differently;
      * ---> mysqli_query returns a "RESOURCE", not a php variable;
      *      e.g. For SHOW TABLES, use mysqli_fetch_row(); 
      *          only row[0] contains anything
+     * ---> these only return "TRUE":
+     *      CREATE, INSERT, UPDATE, DELETE, DROP4
      */
-    $req = mysqli_query($link,"SHOW TABLES");
+    
+    $tbl = mysqli_query($link,"CREATE TABLE tst(id int, name varchar(30));");
+    if (!$tbl) {
+        die("<p>CREATE TABLE failed</p>");
+    }
+    $req = mysqli_query($link,"SHOW TABLES;");
     if (!$req) {
         die("<p>DB Request Failed: SHOW TABLES</p>");
     }
-    echo "Opened";
+    
 
     echo "<p>Results from SHOW TABLES:</p><ul>";
     while ($row = mysqli_fetch_row($req)) {
@@ -31,6 +38,8 @@
         echo "<li>" . $row[0] . "</li>";
     }
     echo "</ul>";
+    
+    
 
     mysqli_close($link);
 ?>
