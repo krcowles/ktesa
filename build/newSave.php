a34f6fa177ad9120e6b5929d14ff4cf51c26baef
Ken Cowles <krcowles29@gmail.com>
2017-09-08 08:28:18 -0600

    fixed multiple behavioral issues with newHike.php and newSave.php resulting in duplicate data in the database: see email 'Hows the trip so far'

--- build/newSave.php
+++ build/newSave.php
@@ -20,25 +20,57 @@
             $database = '../data/database.xml';
             $hikeIndx = intval(filter_input(INPUT_GET,'newno'));
             $newHike = filter_input(INPUT_GET,'new');
+            /*
+             * NOTE: In the first part of this script, simplexml is not used,
+             * but rather a basic read/write of the database file. This is to 
+             * take advantage of an existing text file "xmlHikeRow.txt" 
+             * which is used to add the required fields for an 'empty' page, 
+             * without having to iterate "simpleXml->addChild() over and over 
+             * again (which incidentally also injects poor formatting).
+             */
+            $xmlfile = fopen($database,"r+");
+            if ($xmlfile === false) {
+                $errmsg = '<p style="color:red;font-size:20px;margin-left:16px">' .
+                    'Mdl - newSave.php: Could not open database for read/write '
+                        . 'to add new row: contact site master</p>';
+                die ($errmsg);
+            }
+            $newrow = file_get_contents('xmlHikeRow.txt');
+            if ($newrow === false) {
+                $errmsg = '<p style="color:red;font-size:20px;margin-left:16px">' .
+                    'Mdl - newSave.php: Could not acquire string xmlHikeRow.txt: '
+                        . 'contact site master</p>';
+                die ($errmsg);
+            }
+            $newrow = "\n" . $newrow;
+            fseek($xmlfile,-8,SEEK_END);
+            fwrite($xmlfile,$newrow);
+            fwrite($xmlfile,"\n</rows>");
+            fclose($xmlfile);
+            
+            # now use simplexml to populate startup fields 
             $xmlDB = simplexml_load_file($database);
             if ($xmlDB === false) {
                 $diemsg = '<p style="color:red;font-size:20px;margin-left:16px">' .
-                    'Could not open database: contact Site Master</p>';
+                    'Mdl - newSave.php: simplexml load failed: contact Site Master</p>';
             }
-            if ($newHike == '') {
+            if ($newHike == '') {  # this should never happen: tested in prev. script
                 $nohike = '<p style="color:red;font-size:20px;margin-left:16px">' .
-                    'Hike name empty or not received: go back and enter a hike name</p>';
+                    ' Mdl - newSave.php: Hike name empty or not received: '
+                        . 'contact site master</p>';
                 die($nohike);
             }
+            $xmlDB->row[$hikeIndx-1]->indxNo = $hikeIndx;
             $xmlDB->row[$hikeIndx-1]->pgTitle = $newHike;
             echo '<h2 style="color:brown">You have successfully created a new '
                 . 'hike for ' . $newHike . " as Hike No. " . $hikeIndx . "</h2>\n"
-                . "<p>You may edit this hike at any time by returning to the "
-                . "home page and selecting 'Edit Hike'</p>\n NOTE: For now, " .
-                "please enter the following into the browser url bar (or click): ";
+                . '<p>[<strong>NOT IMPLEMENTED YET:</strong>] You may edit this '
+                . 'hike at any time by returning to the home page and selecting '
+                . '"Edit Hike"</p><p style="font-size:20px;">NOTE: For now, '
+                . 'please enter the following into the browser url bar (or click):<br />';
             echo '<a style="color:darkBlue;text-indent:24px;font-size:18px;" ' .
                     'href="enterHike.php?hikeNo=' . $hikeIndx .
-                    '" target="_blank">' . 'localhost/build/enterHike.php?hikeNo=' . 
+                    '" target="_blank">[server]/[project]/build/enterHike.php?hikeNo=' . 
                     $hikeIndx . "</a>";
             $xmlDB->asXML($database);
         ?>
