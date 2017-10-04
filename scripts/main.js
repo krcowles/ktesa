7e4b0476f87bc57d632c7ffc987844bf1a1437e0
Ken Cowles <krcowles29@gmail.com>
2017-10-03 19:09:29 -0600

    first pass at adding user sign-up

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,47 @@
 $( function() {  // wait until document is loaded...
 
+var init = $('#regusrs').css('display');
+alert("INIT: " + init);
 $('#auxfrm').submit( function() {
-	var keyval = this.regkey.value;
-	if (keyval == '1948') {
-		$('#experts').css('display','block');
-		$('#editor').on('click', function() {
-			var editUrl = 'build/hikeEditor.php';
-			window.open(editUrl, target="_blank");
-		});
-		$('#indxpg').on('click', function() {
-			var indxurl = 'build/indexEditor.php';
-			window.open(indxurl, target="_blank");
-		});
-		$('#creator').on('click', function() {
-			var createUrl = 'build/newHike.php';
-			window.open(createUrl, target="_blank");
-		});
-                $('#admin').on('click', function() {
-                    var adminUrl = 'admin/admintools.php';
-                    window.open(adminUrl,target="_blank");
-                });
-		$('#hide').on('click', function() {
-			$("input[type='password']").val('');
-			$('#experts').css('display','none');
-		});
-	} else {
-		window.alert("Key not recognized");
-	}
-	return false;
+    var keyval = this.regkey.value;
+    if (keyval == '1948') {
+        $('#regusrs').css('display','block');
+        $('#masters').css('display','none');
+        $('#creator').on('click', function() {
+            var createUrl = 'build/newHike.php';
+            window.open(createUrl, target="_blank");
+        });
+        $('#editor').on('click', function() {
+            var editUrl = 'build/hikeEditor.php';
+            window.open(editUrl, target="_blank");
+        });
+        $('.hide').on('click', function() {
+            $("input[type='password']").val('');
+            $('#regusrs').css('display','none');
+            
+        });
+    } else if (keyval == '000ktesa9') {
+        $('#regusrs').css('display','none');
+        $('#masters').css('display','block');
+        $('#indxpg').on('click', function() {
+            var indxurl = 'build/indexEditor.php';
+            window.open(indxurl, target="_blank");
+        });  
+        $('#admin').on('click', function() {
+            var admintools = 'admin/admintools.php';
+            window.open(admintools,"_blank");
+        });
+        $('.hide').on('click', function() {
+            $("input[type='password']").val('');
+            $('#masters').css('display','none');
+        });
+    } else {
+        $("input[type='password']").val('');
+        window.alert("Key not recognized");
+    }
+    return false;
 });
 // IF KEY SUCCESSFUL:
-
-
 $('#turnon').on('click', function() {
 	$('#more').css('display','block');
 	$(this).css('display','none');
