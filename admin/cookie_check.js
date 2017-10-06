0a96fd3b84d049faaa340176b2099d4b4909c26e
Ken Cowles <krcowles29@gmail.com>
2017-10-06 15:27:45 -0600

    completed user validation and cookie-set (turned off right now), verified via new user registration

---
+++ admin/cookie_check.js
@@ -0,0 +1,18 @@
+$(function () { // when page is loaded...
+
+var cookies = navigator.cookieEnabled ? true : false;
+if (!cookies) {
+    var addendum = 'Cookies are not enabled on your machine, therefore ' +
+        'you will also be required to enter your user password';
+    $('#cookies').append(addendum);
+    $('#cookies').after('<br />');
+} else {
+    var d = new Date();
+    d.setTime(d.getTime() + (365*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    //document.cookie = 'nmh_id=' + $('#uname').text() + ";" + expires + ";path=/";
+}
+
+});
+
+
--- admin/create_user.php
+++ admin/create_user.php
@@ -3,7 +3,8 @@ require_once "../mysql/local_mysql_connect.php";
 $fname = mysqli_real_escape_string($link,filter_input(INPUT_POST,'firstname'));
 $lname = mysqli_real_escape_string($link,filter_input(INPUT_POST,'lastname'));
 $uname = mysqli_real_escape_string($link,filter_input(INPUT_POST,'usr'));
-$pword = mysqli_real_escape_string($link,crypt(filter_input(INPUT_POST,'password')));
+$tpass = filter_input(INPUT_POST,'password');
+$pword = mysqli_real_escape_string($link,password_hash($tpass,PASSWORD_DEFAULT));
 $email = filter_input(INPUT_POST,'email',FILTER_VALIDATE_EMAIL);
 if (!$email) {
     echo "Invalid email address - please go back to the Registration Page";
@@ -25,20 +26,61 @@ if ($month > 6) {
 }
 $exp_date = $year . "-" . $month . "-" . $day;
 $passwd_exp = mysqli_real_escape_string($link,$exp_date);
-$newuser = "INSERT INTO USERS " .
-    "(username,passwd,passwd_expire,"
-        . "last_name,first_name,email,facebook_url,"
-        . "twitter_handle,bio) " .
-    "VALUES ( '{$uname}','{$pword}','{$passwd_exp}',"
-        . "'{$lname}','{$fname}','{$email}','{$facbk}',"
-        . "'{$twitt}','{$binfo}');";
-$insert = mysqli_guery($link,$newuser);
+$newuser = "INSERT INTO USERS (username,passwd,passwd_expire,last_name," .
+        "first_name,email,facebook_url,twitter_handle,bio) " .
+    "VALUES ('{$uname}','{$pword}','{$passwd_exp}','{$lname}','{$fname}'," .
+            "'{$email}','{$facbk}','{$twitt}','{$binfo}');";
+$insert = mysqli_query($link,$newuser);
+/*
 if (!insert) {
     if (Ktesa_Dbug) {
         debug_print("Could not insert new user info: " . mysqli_error());
     } else {
         user_error_msg('../mysql/',2,0);
     }
+} else {
+    echo '<p style="display:none;" id="uname">' . $uname . '</p>';
 }
-# HTML FOR SUCCESS HERE....
+ * 
+ */
 ?>
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <title>Successful Registration</title>
+    <meta charset="utf-8" />
+    <meta name="description" content="Successful Registration" />
+    <meta name="author" content="Tom Sandberg and Ken Cowles" />
+    <meta name="robots" content="nofollow" />
+    <link href="../styles/logo.css" type="text/css" rel="stylesheet" />
+    <style type="text/css">
+        body { background-color: #eaeaea; }
+    </style>
+</head>
+<body>
+<div id="logo">
+    <img id="hikers" src="../images/hikers.png" alt="hikers icon" />
+    <p id="logo_left">Hike New Mexico</p>
+    <img id="tmap" src="../images/trail.png" alt="trail map icon" />
+    <p id="logo_right">w/Tom &amp; Ken</p>
+</div>
+<p id="trail">Registration Complete</p>
+<div style="margin-left:24px;">
+    <h1>
+        You have successfully registered!
+    </h1> 
+    <h3>
+        You may now display registered user options on the main page to
+        create and edit hikes.<br /><br />You are now logged in and can display
+        user options by entering your user name in the provided field.<br />
+        <em id="cookies" style="color:brown;"></em><br />
+        Click on the link below:
+    </h3>
+    <p style="font-size:18px;color:brown;">
+        <a href="../index.html">Main Page Link</a><br />
+    </p>
+</div>
+<script src="../scripts/jquery-1.12.1.js"></script>
+<script src="cookie_check.js"></script>
+</body>
+</html>
