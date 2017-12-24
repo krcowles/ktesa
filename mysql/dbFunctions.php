40ebe6566627ef26e1e3cdb8acf84f64098a6f82
tjsandberg <tjsandberg@yahoo.com>
2017-12-24 07:15:52 -0700

    Split setenv.php into two files in order to comply with PSR1 coding standard.

---
+++ mysql/dbFunctions.php
@@ -0,0 +1,81 @@
+<?php
+function insertDbRow($link, $table, $file, $line)
+{
+    $insQuery = "INSERT INTO {$table} () VALUES ();";
+    $insResults = mysqli_query($link, $insQuery);
+    if (!$insResults) {
+        echo "insQuery: {$insQuery} <br>";
+        die("Function insertDbRow failed to insert into table: {$table} when called from file {$file}: line: {$line}: "
+        . mysqli_error($link));
+    }
+    mysqli_free_result($insResults);
+
+    # Get index number
+    $selQuery = "SELECT * FROM {$table} ORDER BY 1 DESC LIMIT 1;";
+    $selResults = mysqli_query($link, $selQuery);
+    $index = mysqli_fetch_row($selResults);
+    if (!$selResults) {
+        echo "selQuery: {$insQuery} <br>";
+        die("Function insertDbRow failed to get index from table: {$table} when called from file {$file}: line: {$line}: "
+        . mysqli_error($link));
+    }
+    mysqli_free_result($selResults);
+    return $index[0];
+}
+function doQuery($link, $query, $file, $line)
+{
+    $results = mysqli_query($link, $query);
+    if (!$results) {
+        die("Function insertDbRowMulti failed when called " .
+        "from file {$file}: line: {$line} " .
+        "with query : {$query} <br> " .
+        mysqli_error($link));
+    }
+    mysqli_free_result($insResults);
+}
+#
+function getDbRowNum($link, $table, $file, $line)
+{
+    # Get index number
+    $query = "SELECT * FROM {$table} ORDER BY 1 DESC LIMIT 1;";
+    $results = mysqli_query($link, $query);
+    if (!$results) {
+        die("Function getDbRowNum failed when called " .
+        "from file {$file}: line: {$line} " .
+        "with query : {$query} <br> " .
+        mysqli_error($link));
+    }
+    $newRow = mysqli_fetch_row($results);
+    mysqli_free_result($results);
+    return $newRow[0];
+}
+#
+function getDbRowCount($link, $query, $file, $line)
+{
+    $results = mysqli_query($link, $query);
+    if (!$results) {
+        die("Function getDbRowCount failed when called " .
+        "from file {$file}: line: {$line} " .
+        "with query : {$query} <br> " .
+        mysqli_error($link));
+    }
+    $rowCount = mysqli_num_rows($results);
+    mysqli_free_result($results);
+    return $rowCount;
+}
+function updateDbRow($link, $table, $row, $field, $indexId, $content, $file, $line)
+{
+    if (is_null($content)) {
+        $query = "UPDATE {$table} SET {$field} = NULL WHERE {$indexId} = {$row};";
+    } else {
+        $content_e = mysqli_real_escape_string($link, $content);
+        $query = "UPDATE {$table} SET {$field} = '{$content_e}' WHERE {$indexId} = '{$row}';";
+    }
+    $result = mysqli_query($link, $query);
+    if (!$result) {
+        echo 'query: ' . $query . '<br>';
+        die("Function updateDbRow failed on table: {$table} when called from file {$file}: line: {$line}: "
+        . mysqli_error($link));
+    }
+    mysqli_free_result($result);
+}
