a5c723613adb2c356e1a7185c762e2f9301e0fc0
Ken Cowles <krcowles29@gmail.com>
2018-02-22 17:21:26 -0700

    basic scripts and ajax working - need to decode returned data and get to the page

--- build/photoLoader.js
+++ build/photoLoader.js
@@ -1,4 +1,23 @@
 $(function () { // when page is loaded...
 // imported vars from php: cnt (int) pgLinks (json array) albTypes (json array)
-alert("Qty: " + cnt + ", Data: " + pgLinks);
+var alinks = JSON.stringify(pgLinks);
+var atypes = JSON.stringify(albTypes);
+var ajaxdata = { 'cnt': cnt, 'albs': alinks, 'types': atypes };
+$.ajax({
+    type: 'POST',
+    url: 'getPicDat.php',
+    dataType: 'text',
+    data: ajaxdata,
+    success: function(result) {
+        var picdata = result;
+        alert("Result: " + picdata);
+        $('#loader').css('display','none');
+        $('#main').css('display','block');
+    },
+    error: function(jq, errmsg, stat) {
+        var emsg = "Failed to execute getPicDat.php: " + errmsg + "; " + stat;
+        alert(emsg);
+    }
+});
+
 });
