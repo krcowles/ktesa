ac415f9c8c66a9b11caada5cce9bd86cc10df934
tjsandberg <tjsandberg@yahoo.com>
2018-04-25 07:54:34 -0600

    Add buildPhar script to archive website using PHP Phar and Zip utilities. Later, we may want to use Phar iterator capability to only include modified files.

---
+++ admin/buildPhar.php
@@ -0,0 +1,16 @@
+<?php
+$tmpFilename = sys_get_temp_dir() . '/archive.tar';
+$phar = new PharData($tmpFilename);
+// add all files in the project and then compress it
+$phar->buildFromDirectory('../');
+$phar->compress(Phar::GZ);
+// Download the zip file
+header("Content-Type: application/x-gtar");
+header("Content-Disposition: attachment; filename='".basename($tmpFilename . '.gz'));
+header("Content-Length: " . filesize($tmpFilename . '.gz'));    
+header("Content-Transfer-Encoding: binary");
+readfile($tmpFilename . '.gz');
+// clean up
+unlink($tmpFilename);
+unlink($tmpFilename . '.gz');
+?>
