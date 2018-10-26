2022cdba45460bbc2f07c49b36a92e1a3ecc7296
Ken Cowles <krcowles29@gmail.com>
2018-10-25 19:03:54 -0600

    first pass basic selection functionality for filtering, but no filtering

--- scripts/tblOnlySort.js
+++ scripts/tblOnlySort.js
@@ -3,6 +3,80 @@
    usrTbl script is applied to a table constructed based on the map viewport. */
 $( function() {  // wait until document is loaded...
 
+// filtering alignment:
+var winwidth = $(window).innerWidth();
+var tblwidth = $('.sortable').width();
+var margs = Math.floor((winwidth - tblwidth)/2) + "px";
+$('#tblfilter').css('margin-left', margs);
+$('#tblfilter').css('margin-right', margs);
+$('#filtnote').css('margin-left', margs);
+$('#filtnote').css('margin-right', margs);
+// filtering functionality:
+$('#showfilter').on('click', function() {
+	$('#dispopts').css('display', 'block');
+});
+$('#loc').on('click', function() {
+	if ($('#selloc').hasClass('hidden')) {
+		$('#selloc').removeClass('hidden');
+		$('#selloc').addClass('inline');
+		$('#loclbl').removeClass('normal');
+		$('#loclbl').addClass('hilite');
+	} else {
+		$('#selloc').removeClass('inline');
+		$('#selloc').addClass('hidden');
+		$('#loclbl').removeClass('hilite');
+		$('#loclbl').addClass('normal');
+		$(this).prop('checked', false);
+	}
+	$('#hike').prop('checked', false);
+	if ($('#selhike').hasClass('inline')) {
+		$('#selhike').removeClass('inline');
+		$('#selhike').addClass('hidden');
+		$('#hikelbl').removeClass('hilite');
+		$('#hikelbl').addClass('normal');
+	}
+});
+$('#hike').on('click', function() {
+	if ($('#selhike').hasClass('hidden')) {
+		$('#selhike').removeClass('hidden');
+		$('#selhike').addClass('inline');
+		$('#hikelbl').removeClass('normal');
+		$('#hikelbl').addClass('hilite');
+	} else {
+		$('#selhike').removeClass('inline');
+		$('#selhike').addClass('hidden');
+		$('#hikelbl').removeClass('hilite');
+		$('#hikelbl').addClass('normal');
+		$(this).prop('checked', false);
+	}
+	if ($('#selloc').hasClass('inline')) {
+		$('#selloc').removeClass('inline');
+		$('#selloc').addClass('hidden');
+		$('#loclbl').removeClass('hilite');
+		$('#loclbl').addClasS('normal');
+	}
+	$('#loc').prop('checked', false);
+});
+$('a').on('click', function(ev) {
+	if ($('#hike').prop('checked')) {
+		ev.preventDefault();
+		var thishike = $(this).text();
+		$('#link').val(thishike);
+	}
+});
+$('#apply').on('click', function() {
+	var epsilon = $('#within').val();
+	// validate
+	if (!$.isNumeric(epsilon)) {
+		alert("Non-numeric entry made in 'Hikes within' text box");
+		return;
+	}
+	var center = "Fred";
+	filterList(epsilon, center);
+});
+function filterList(range, pivot) {
+	alert("Hikes within " + range + " miles of " + pivot);
+}
 // global object used to define how table items get compared in a sort:
 var noPart1;
 var noPart2;
