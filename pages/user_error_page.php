7dbe97232ad3e95d5d8aecdd2019b8b84dadfafb
Ken Cowles <krcowles29@gmail.com>
2018-11-10 17:55:30 -0700

    added the beginnings of a user-friendly error page

---
+++ pages/user_error_page.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <title>Error Encountered</title>
+    <meta charset="utf-8" />
+    <meta name="description" content="User Error Notification" />
+    <meta name="author" content="Tom Sandberg and Ken Cowles" />
+    <meta name="robots" content="nofollow" />
+    <link href="../styles/logo.css" type="text/css" rel="stylesheet" />
+    <link href="../styles/user_errors.css" type="text/css" rel="stylesheet" />
+    <style type="text/css">
+        body { background-color: #eaeaea; };
+    </style>
+</head>
+
+<body>
+<div id="logo">
+	<img id="hikers" src="../images/hikers.png" alt="hikers icon" />
+	<p id="logo_left">Hike New Mexico</p>	
+	<img id="tmap" src="../images/trail.png" alt="trail map icon" />
+	<p id="logo_right">w/Tom &amp; Ken</p>
+</div>
+<p id="trail">Problem Encountered</p>
+<div id="notice">
+    We are sorry, but an error has occurred on the site. The details
+    of this error have been sent to the site masters, and will be addressed
+    as soon as possible.<br /><br />Thanks for your patience!
+</div>
+</body>
+
+</html>
