155ea23cd21f5997b2db4d67a0248b4dc7d70cce
Ken Cowles <krcowles29@gmail.com>
2018-11-15 16:07:17 -0700

    first pass realistic global_boot file and user error page

--- php/global_boot.php
+++ php/global_boot.php
@@ -4,74 +4,47 @@
  * to be 'required' by all session-creating php modules, as necessary.
  * It includes function definitions used by many modules as well as error
  * reporting and logging options, whether in development or production mode.
- * It also estabishes the PDO object for the session.
+ * It also establishes the PDO object for the session.
  * PHP Version 7.1
  * 
  * @package Global_Boot
  * @author  Tom Sandberg and Ken Cowles <krcowles29@gmai.com>
  * @license No license to date
  */
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-// test control:
-$mono = false;
-$whps = false;
-$errh = false;
-$exch = false;
-
-require "../../settings.php";
-// Error settings:
-error_reporting(2147483647); // recommended by PHP site instead of E_ALL for future vals
-ini_set('log_errors', 1);
-ini_set('error_log', '../ktesa.log');
-
-//ini_set('display_errors', 'on'); // default is off 
-//ini_set('display_startup_errors', 1);  // should never be 'on' in production
-// Error and exception packages:
 require "../vendor/autoload.php";
-if ($mono) {
-    $log = new Logger('ktesa');
-    $log->pushHandler(new StreamHandler('../ktesa.log', Logger::WARNING));
-}
-/** 
- * The error_handler negates instantiation of whoops (on errors) and is only
- * set in production mode. The exception handler does not negate instatiation
- * whoops (on exceptions), though whoops precludes it.
- * Error logs are kept in ktesa.log
- */
-if ($whps) {
+require "../../settings.php";
+
+error_reporting(2147483647); // PHP site recommends for future expansion of E_ALL
+if ($appMode === 'production') {
+    ini_set('log_errors', 1);
+    ini_set('error_log', '../ktesa.log');
+    set_error_handler('ktesa_errors');
+    set_exception_handler('ktesa_exceptions');
+} else {
+    ini_set('display_errors', 'on'); // default is off i.e. 'production'
+    ini_set('display_startup_errors', 1);  // should never be 'on' in production
     $whoops = new \Whoops\Run;
     $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
     $whoops->register();
-} 
-if ($errh) {
-    set_error_handler('ktesa_errors');
-}
-if ($exch) {
-    set_exception_handler('ktesa_exceptions');
 }
 function ktesa_errors($errno, $errstr, $errfile, $errline) {
     $message = "An error occurred in {$errfile} on line {$errline}. " .
         "<br />Error no. {$errno}: {$errstr}<br /><br />";
     echo $message;
-    //error_log($message."\n", 3, '../ktesa.log');
+    error_log($message."\n", 3, '../ktesa.log');
+    error_page();
     die("Execution halted");
 }
 function ktesa_exceptions($exception) {
-    // Note: handler will not be invoked when whoops is active
     // execution halts automatically after the uncaught exception: no 'die' needed.
     $message = "An uncaught exception occurred:<br />" . $exception->getMessage();
-    echo $message;
+    error_log($message);
+    error_page();
 } 
-
-// error tests:
-$handle = fopen("asdf"); // create an error with this command
-//trigger_error("This is a test; this is ONLY a test!");
-//$log->warning("try this...");
-//throw new Exception("Test throw");
-
-
-
+function error_page() {
+    $user_error_page = "user_error_page.php";
+    header("Location: {$user_error_page}");
+}
 
 // Function definitions:
 require "../admin/adminFunctions.php";
---
+++ php/user_error_page.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html lang="en-us">
+
+<head>
+    <title>Error Encountered</title>
+    <meta charset="utf-8" />
+    <meta name="description" content="User notice of problem encountered" />
+    <meta name="author" content="Tom Sandberg and Ken Cowles" />
+    <meta name="robots" content="nofollow" />
+    <link href="../styles/logo.css" type="text/css" rel="stylesheet" />
+    <style type="text/css">
+        body { background-color: #eaeaea; };
+    </style>
+</head>
+
+<body>
+<div id="logo">
+	<img id="hikers" src="../images/hikers.png" alt="hikers icon" />
+	<p id="logo_left">Hike New Mexico</p>	
+	<img id="tmap" src="../images/trail.png" alt="trail map icon" />
+	<p id="logo_right">w/Tom &amp; Ken</p>
+</div>
+<p id="trail">Problem Encountered</p>
+
+<div style="margin-left:16px;font-size:18px;color:brown">
+    <p style="margin:0;font-weight:bold;">We are sorry, but a problem has
+        occurred while processing your request.</p>
+    <p style="margin:0;text-indent:30px;color:black;">An email has been
+        sent to the web master, and the problem will be investigated promptly.
+    </p>
+    <p>You may wish to try again at a later date/time. Thanks for your patience!</p>   
+</div>
+
+</body>
+
+</html>
