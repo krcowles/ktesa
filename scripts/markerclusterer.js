237b29b06a4cbc199a0e29446378454758fcb2f5
Ken Cowles <krcowles29@gmail.com>
2018-12-17 11:13:30 -0700

    converted to white text in clusterer

--- scripts/markerclusterer.js
+++ scripts/markerclusterer.js
@@ -1254,7 +1254,7 @@ ClusterIcon.prototype.createCss = function(pos) {
         this.height_ + 'px; width:' + this.width_ + 'px; text-align:center;');
   }
 
-  var txtColor = this.textColor_ ? this.textColor_ : 'black';
+  var txtColor = this.textColor_ ? this.textColor_ : 'white';
   var txtSize = this.textSize_ ? this.textSize_ : 11;
 
   style.push('cursor:pointer; top:' + pos.y + 'px; left:' +
