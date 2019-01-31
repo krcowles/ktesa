940e05e4e5ec011cd886c50009182ad646c3ae66
Ken Cowles <krcowles29@gmail.com>
2019-01-30 19:20:03 -0700

    separated functions out for later inclusion in separate file

--- php/global_boot.php
+++ php/global_boot.php
@@ -4,16 +4,15 @@
  * is to be 'required' by all session-creating php modules.
  * It includes function definitions used by many modules as well as 
  * error reporting and logging options, whether in development or 
- * production mode. [The error reporting/logging will not be fully
- * incorporated until the PDO migration is complete.]
- * This file also establishes the PDO object for the session ($pdo).
+ * production mode. This file also establishes the PDO object for
+ * the session ($pdo).
  * PHP Version 7.1
  * 
  * @package Global_Boot
  * @author  Tom Sandberg and Ken Cowles <krcowles29@gmail.com>
  * @license No license to date
  */
-require "../vendor/autoload.php";
+require "../vendor/autoload.php"; // CHECK OTHER FILES FOR REPEAT!!
 require "../admin/mode_settings.php"; // Capture this code version's settings
 $settings = $_SERVER["DOCUMENT_ROOT"] . "/../settings.php";
 require $settings;
@@ -45,8 +44,8 @@ error_reporting(2147483647); // PHP site recommends for future expansion of E_AL
 if ($appMode === 'production') {
     ini_set('log_errors', 1); // this may be the default anyway
     ini_set('error_log', '../ktesa.log');
-    set_error_handler('ktesa_errors'); // errors not using Throwable interface
-    set_exception_handler('ktesa_exceptions'); // uncaught exceptions
+    set_error_handler('ktesaErrors'); // errors not using Throwable interface
+    set_exception_handler('ktesaExceptions'); // uncaught exceptions
 } else { // development
     // in this mode, no error_log is specified, so we use syslog
     ini_set('display_errors', 'on'); // default is off i.e. 'production'
@@ -55,22 +54,51 @@ if ($appMode === 'production') {
     $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
     $whoops->register();
 }
-// production mode error handlers:
-function ktesa_errors($errno, $errstr, $errfile, $errline) {
+// belongs in separate file when done per PSR1
+
+/**
+ * This function establishes production mode error handling, which
+ * will utilize a user-friendly error page.
+ * 
+ * @param string $errno   The error number reported back by the error
+ * @param string $errstr  The actual error message reported
+ * @param string $errfile The file name in which the error occurred
+ * @param string $errline The line in the above file in which error occurred
+ * 
+ * @return null
+ */
+function ktesaErrors($errno, $errstr, $errfile, $errline)
+{
     $message = "A ktesa error occurred in {$errfile} on line {$errline}. " .
         "<br />Error no. {$errno}: {$errstr}<br /><br />";
     error_log($message."\n", 3, '../ktesa.log'); // eventually, switch to email?
     error_page();
     die("ERROR: Execution halted"); // die is required in this handler
 }
-function ktesa_exceptions($exception) {
-    // execution halts automatically after the uncaught exception: no 'die' needed.
-    $message = "A KTESA uncaught exception occurred:<br />" . $exception->getMessage();
+/**
+ * This is the production mode exception handler, also presenting 
+ * exception data to the logger and a user-friendly page to the user.
+ * Note that execution halts automatically after the uncaught exception.
+ * 
+ * @param object $exception The exception object
+ * 
+ * @return null
+ */
+function ktesaExceptions($exception)
+{
+    $message = "A KTESA uncaught exception occurred:<br />" .
+        $exception->getMessage();
     error_log($message);
     // ktesa.log should show trace
-    error_page();
+    errorPage();
 } 
-function error_page() {
+/**
+ * This is the user-friendly error page presented to the user
+ * 
+ * @return null
+ */
+function errorPage()
+{
     $user_error_page = "user_error_page.php";
     header("Location: {$user_error_page}");
 }
@@ -97,7 +125,8 @@ catch (Throwable $t) {
 }
 catch (Error $er) {
     die("Old error class?");
-    //$msg = "Is there a throwable i/f? " . $er->getFile() . " at line " . $er->getLine();
+    //$msg = "Is there a throwable i/f? " . $er->getFile() .
+        " at line " . $er->getLine();
     //die($msg);
 }
 catch ( DivsionByZeroError $d) {
