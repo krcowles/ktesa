9a648f8a75d130c8c845cacf67bad512de7031d2
Ken Cowles <krcowles29@gmail.com>
2019-02-25 14:03:33 -0700

    tab2 visual enhancements

--- build/tab2display.php
+++ build/tab2display.php
@@ -1,14 +1,12 @@
 <hr />
-<h3>Upload your photos directly! 
-<input type="button" name="upld" id="upld" value="Go to Upload Pg" /></h3>
+<p style="color:brown;">Upload your photos (you will be directed to a new page)</p> 
+<input type="button" name="upld" id="upld" value="Upload" /></h3>
 <hr />
 <p style="color:brown;"><em>Edit captions below each photo as needed
     and assign display options.</em></p>
 <input type="hidden" name="pno" value="<?php echo $hikeNo;?>" />
 <input type="hidden" name="pid" value="<?php echo $uid;?>" />
 <div style="margin-left:8px;">
-    <p style="font-size:20px;font-weight:bold;">Apply the
-        Photo Assignments Below&nbsp;
         <input type="submit" name="savePg" value="Apply" /></p>
 </div>
 <?php
