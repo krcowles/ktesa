7725dcf663f7957e9bc25b93f3ce80e504340b6d
Ken Cowles <krcowles29@gmail.com>
2019-03-07 17:54:22 -0700

    added mapping of edited waypoints to makeGpsv.php

--- php/makeGpsv.php
+++ php/makeGpsv.php
@@ -209,7 +209,7 @@ for ($i=1; $i<count($allLngs)-1; $i++) {
 $clat = $south + ($north - $south)/2;
 $clon = $west + ($east - $west)/2;
 /*
- *   ---- ESTABLISH ANY WAYPOINTS ----
+ *   ---- ESTABLISH ANY WAYPOINTS IN gpx FILE ----
  */
 $noOfWaypts = $gpxdat->wpt->count();
 $waypoints = [];
@@ -259,10 +259,19 @@ if ($showPhotos) {
                 $iconColor = $defIconColor;
             }
             // If wypt in ETSV file....
-            if ($photos['mid'] == '') { // waypoint icon
+            if (empty($photos['mid'])) { // waypoint icon
+                // determine icon color and style (clumsy for now, but works)
+                $colortxt = $iconColor;
+                $iconStyle = $iconColor;
+                if (strpos($colortxt, "Blue") || strpos($colortxt, "Trail Head")) {
+                    $iconColor = 'Blue';
+                } elseif (strpos($iconColor, "Green")) {
+                    $iconColor = 'Green';
+                } 
                 $plnk = "GV_Draw_Marker({lat:" . $photos['lat'] . ",lon:" .
                     $photos['lng']. ",name:'" . $procName . "',desc:'" .
-                    $procDesc . "',color:'" . $iconColor . "',icon:''});";
+                    $procDesc . "',color:'" . $iconColor . 
+                        "',icon:'" . $iconStyle . "'});";
             } else { // photo
                 $plnk = "GV_Draw_Marker({lat:" . $photos['lat'] . ",lon:" .
                     $photos['lng'] . ",name:'" . $procDesc .
