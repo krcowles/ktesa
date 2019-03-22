4850df732115f053f83132d8835086f1a145a126
Ken Cowles <krcowles29@gmail.com>
2019-03-21 18:52:17 -0600

    functionality in place for 2-level sorts

--- scripts/multi-sort.js
+++ scripts/multi-sort.js
@@ -8,6 +8,16 @@
  */
 var indx;
 var compareType;
+var icon = false;  // true when <td> contains image for exposure
+function iconType(imgsrc) {
+    if (imgsrc.indexOf('fullSun') !== -1) {
+        return 'Sun';
+    } else if(imgsrc.indexOf('partShade') !== -1) {
+        return 'Partial';
+    } else {
+        return 'Shady';
+    }
+}
 $('#sort').on('click', function() {
     // validate sort and depth of sort
     var key1 = $('#sort1').val();
@@ -32,21 +42,71 @@ $('#sort').on('click', function() {
     }
     var $headers = $('.fsort').find('th');  // contains sort type
     // level 1 sort:
+    if (level1 === 'Exposure') {
+        icon = true;
+    }
     $headers.each(function(index) {
         if ($(this).text() === level1) {
             indx = index;
             compareType = $(this).data('sort');
+            if (typeof(compareType == 'undefined')) {
+                compareType = 'std';  // exposure has no data-sort type
+            }
             return true;
         }
     });
     rows.sort(function(a, b) {
-        a = $(a).find('td').eq(indx).text();
-        b = $(b).find('td').eq(indx).text();
+        if (icon) {
+            var imga = $(a).find('td').eq(indx).children();
+            var imgb = $(b).find('td').eq(indx).children();
+            var srca = $(imga[0]).attr('src');
+            var srcb = $(imgb[0]).attr('src');
+            a = iconType(srca);
+            b = iconType(srcb);
+        } else {
+            a = $(a).find('td').eq(indx).text();
+            b = $(b).find('td').eq(indx).text();
+        }
         return compare[compareType](a, b);
     });
-    $tbody.append(rows);
     // level 2 sort:
     if (level2) {
-
+        var subrows = [];
+        var lastkey = indx;  // this is the <td> on which level1 sorted
+        var subsort = $(rows[0]).find('td').eq(lastkey).text();
+        $headers.each(function(index) {
+            if ($(this).text() === level2) {
+                indx = index;
+                compareType = $(this).data('sort');
+            }
+        });
+        var k = 0;
+        while (k<rows.length) {
+            if($(rows[k]).find('td').eq(lastkey).text() === subsort) {
+                subrows.push(rows[k]);
+                k++;
+            } else {
+                subsort = $(rows[k]).find('td').eq(lastkey).text();
+                if (k === rows.length -1) {  // if the final entry
+                    subrows.push(rows[k]);
+                }
+            }
+        }
+        subrows.sort(function(a, b) {
+            if (icon) {
+                var imga = $(a).find('td').eq(indx).children();
+                var imgb = $(b).find('td').eq(indx).children();
+                var srca = $(imga[0]).attr('src');
+                var srcb = $(imgb[0]).attr('src');
+                a = iconType(srca);
+                b = iconType(srcb);
+            } else {
+                a = $(a).find('td').eq(indx).text();
+                b = $(b).find('td').eq(indx).text();
+            }
+            return compare[compareType](a, b);
+        });
+        rows = subrows.slice(0);
     }
+    $tbody.append(rows);
 });
