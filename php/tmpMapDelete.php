5bb98f7435067217244b5d05c19ee90c773fbdfa
Ken Cowles <krcowles29@gmail.com>
2020-01-09 16:27:31 -0700

    corrected syntax of tmpMapDelete.php and updated with fileDocs

--- php/tmpMapDelete.php
+++ php/tmpMapDelete.php
@@ -1,6 +1,14 @@
 <?php
-    echo
-    $tmpFile = filter_input(INPUT_GET, 'file');
-    if (file_exists($tmpFile)) {
-        unlink($tmpFile);
-    }
+/**
+ * This simple script is ajaxed by any hike page to delete its temporary
+ * map file. The map file is created for the purpose of invoking an iframe.
+ * PHP Version 7.1
+ * 
+ * @package Page_Display
+ * @author  Tom Sandberg and Ken Cowles <krcowles29@gmail.com>
+ * @license No license to date
+ */
+$tmpFile = filter_input(INPUT_GET, 'file');
+if (file_exists($tmpFile)) {
+    unlink($tmpFile);
+}
