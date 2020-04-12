0784fd180edd09064c191a4b51e5f09a79f1aab9
Ken Cowles <krcowles29@gmail.com>
2020-04-12 10:55:02 -0600

    modified upload of photos to eliminate resizing and storing 'nsize' photos

--- build/usrPhotos.php
+++ build/usrPhotos.php
@@ -27,19 +27,11 @@ $n_size = 320;
 $z_size = 640;
 // translate ht/wd into new n-size/z-size dimensions:
 $aspect = $orgHeight/$orgWidth;
-$imgWd_n = $n_size;
-$imgHt_n = intval($n_size * $aspect);
 $imgWd_z = $z_size;
 $imgHt_z = intval($z_size * $aspect);
-// Size width definitions:
-$n_size = 320;
+// Size width definition:
 $z_size = 640;
-// translate ht/wd into new n-size/z-size dimensions:
-$aspect = $orgHeight/$orgWidth;
-$imgWd_n = $n_size;
-$imgHt_n = intval($n_size * $aspect);
-$imgWd_z = $z_size;
-$imgHt_z = intval($z_size * $aspect);
+
 // determine next 'thumb' value for new entry
 $tval = "SELECT `thumb` FROM `TSV` ORDER BY CAST(thumb AS UNSIGNED) DESC LIMIT 1;";
 $tresult = $pdo->query($tval);
@@ -54,14 +46,7 @@ $rotate = false;
 if ($orient == '6') {
     $rotate = true;
 }
-$nfileName = $imgName . "_" . $newthumb . "_n.jpg";
 $zfileName = $imgName . "_" . $newthumb . "_z.jpg";
-$size = "n";
-
-// see buildFunctions.php: note the class SimpleImage checks for GD support
-storeUploadedImage(
-    $nfileName, $photo, $imgWd_n, $imgHt_n, $rotate, $size
-);
 $size = "z";
 storeUploadedImage(
     $zfileName, $photo, $imgWd_z, $imgHt_z, $rotate, $size
