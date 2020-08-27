695f338c1569b46e155996d5914492415fdc607f
tjsandberg <tjsandberg@yahoo.com>
2020-08-27 16:56:55 -0600

    Enhance shutdownHandler to account for case when error_get_last() returns null.

--- php/errFunctions.php
+++ php/errFunctions.php
@@ -57,20 +57,22 @@ function ktesaExceptions($exception)
 function shutdownHandler() //will be called when php script ends.
 {
     $lasterror = error_get_last();
-    switch ($lasterror['type'])
-    {
-    case E_ERROR:
-    case E_CORE_ERROR:
-    case E_COMPILE_ERROR:
-    case E_USER_ERROR:
-    case E_RECOVERABLE_ERROR:
-    case E_CORE_WARNING:
-    case E_COMPILE_WARNING:
-    case E_PARSE:
-        $error = "[SHUTDOWN] lvl:" . $lasterror['type'] .
-            " | msg:" . $lasterror['message'] . " | file:" .
-            $lasterror['file'] . " | ln:" . $lasterror['line'];
-        shutdownError($error, "fatal");
+    if (!empty($lasterror)) {
+        switch ($lasterror['type'])
+        {
+        case E_ERROR:
+        case E_CORE_ERROR:
+        case E_COMPILE_ERROR:
+        case E_USER_ERROR:
+        case E_RECOVERABLE_ERROR:
+        case E_CORE_WARNING:
+        case E_COMPILE_WARNING:
+        case E_PARSE:
+            $error = "[SHUTDOWN] lvl:" . $lasterror['type'] .
+                " | msg:" . $lasterror['message'] . " | file:" .
+                $lasterror['file'] . " | ln:" . $lasterror['line'];
+            shutdownError($error, "fatal");
+        }
     }
 }
 /**
