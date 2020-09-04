e4ec889f0d8275039928cf6a7eb1fe905bb9ba0a
Ken Cowles <krcowles29@gmail.com>
2020-09-04 16:12:58 -0600

    corrected error in distance function (prepareTracks.js) for elevation chart mileage calculations

--- scripts/prepareTracks.js
+++ scripts/prepareTracks.js
@@ -125,8 +125,7 @@ function distance(lat1, lon1, lat2, lon2, unit) {
     var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
     dist = Math.acos(dist);
     dist = dist * 180/Math.PI;
-    dist = dist * 60 * 1.1515;
-    if (unit === "K") { dist = dist * 1.609344; }
-    if (unit === "M") { dist = dist * 0.8684; }  // else result is in miles "M"
+    dist = dist * 60 * 1.1515; // Miles
+    if (unit === "K") { dist = dist * 1.609344; } // Kilometers
     return dist;
 }
