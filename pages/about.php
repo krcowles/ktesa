138eda12465dea94730d5c885ae87be465506c14
Ken Cowles <krcowles29@gmail.com>
2021-04-08 18:48:22 -0600

    updated about page to correspond to recent changes

--- pages/about.php
+++ pages/about.php
@@ -43,7 +43,7 @@ require "../php/global_boot.php";
         a look at a variety of hikes, all undertaken by the authors in 
         <em>The Land of Enchantment</em>. These hikes cover a broad, cross-state
         array of possibilities, all found in the diverse terrain of New Mexico -
-        from short treks, to longer, uphill/downhill ascents.
+        from short treks, to longer, uphill/downhill adventures.
         </p> 
         <p id="features">
             One of the features of this site 
@@ -63,16 +63,18 @@ require "../php/global_boot.php";
         purple marker will display an information window about the hike(s). The home
         page also shows a side table of all hikes appearing in the viewable portion
         of the map. You may zoom in on any given hike, mark it as a favorite, or go
-        directly to the hike page. You may change the table's width by dragging it's
-        left edge.
+        directly to the hike page. You may change the side table's width by dragging
+        it's left edge.
         </p>
         
         <p>There are several menu options at the top of each page allowing you
-        to view other portions of the site: a 'Table Only' page, described below;
-        a 'Show Favorites' page, which will show on a map all hikes in your list
-        of favorites, along with their hike tracks; or, if you have registered on
-        this site (no cost or obligations), you may create and/or edit any hike
-        you have created.
+        to use other portions of the site. The main menu item, the 'Explore' menu,
+        provides several viewing options. In addition to the home page described
+        above, a 'Table Only' page is available. This is further described below;
+        for members (you can sign up for free, and with no obligations), you will
+        also see a 'Show Favorites' page option in the 'Explore' menu, which will
+        show on a map all hikes in your list of favorites, along with their hike
+        tracks;
         </p>
         <div id="m" class="twisty-right"></div>
         <p id="mapfeat" class="maindisp">&nbsp;Home Page Map Features
@@ -82,8 +84,9 @@ require "../php/global_boot.php";
                     display.
             </li>
             <li>Marker pins (purple) display "info-windows" when clicked, showing
-                    hike details, and containing links to the corresponding website
-                    and Google directions to the location of the hike's trail head.
+                hike details, and containing links to the corresponding hike
+                page, as well as Google directions to the location of the hike's
+                trail head.
             </li>
             <li>When zoomed in sufficiently, tracks indicating the trail or path
                     the authors took will display, with arrows indicating
@@ -95,23 +98,31 @@ require "../php/global_boot.php";
         <p id="tblfeat" class="maindisp">&nbsp;Table Only Features:</p>
         <ul id = 'tul' class="dashed">
             <li>The table of hikes is intended to provide a brief summary of hike
-                statistics, and also links to the corresponding hike page, as well
-                as Google driving directions.
+                statistics, and links to the corresponding hike page, along
+                with Google driving directions.
             </li>
-            <li>The table of hikes initially displayed is not sorted in any
-                particular order, but clicking on a column header allows alphabetical
-                (or numerical) sorting. The first click will sort in ascending order,
-                each successive click will reverse the sort order.
+            <li>The table of hikes initially displayed is sorted in ascending
+                alphabetic order, but clicking on any column header (excepting
+                the 'By Car' directions column) allows alphabetical (or numerical)
+                sorting of the column. The first click will sort in ascending
+                order, each successive click will reverse the sort order.
             </li>
-            <li>As you move your mouse over different rows, that row will be
-                highlighted for easy reference.
-            </li>
-            <li> By checking hike boxes, you can then 'Draw' (button) a map
-                which includes only the checked boxes.
-            </li>
+            <li>You will see a 'Table Options' header above the table, offering
+                different ways to view the data, described next:
             <li>You can filter the table of hikes by radial distance from either
                 a location (in the drop-down box), or a specific hike. The resulting
                 table may then be sorted according to hike details provided.
+            </li>
+            <li>You may wish to see selected hikes of your choosing displayed on
+                a separate map. Simply type in the hike names requested, then
+                click on 'Draw Map'.
+            </li>
+            <li>You can convert between English and Metric units separately on the
+                main table or on filtered tables.
+            </li>
+            <li>
+                As the table is rather large, a 'speedy scroller' drop-down is
+                provided to quickly place you within easy reach of your target.
             </li>
         </ul>
         <div id="h" class="twisty-right"></div>
@@ -123,6 +134,18 @@ require "../php/global_boot.php";
             </li>
             <li>As you cursor along the chart, the corresponding hike location
                 shows up on the local map.
+            </li>
+            <li>For hikes with multiple tracks, the tracks will be available for
+                display by clicking on their respective checkboxes in the 'track
+                box' area, upper right corner of the map. The topmost track displayed
+                will have its data reflected in the sidebar statistics, and will be
+                the track displayed in the elevation chart.
+            </li>
+            <li>If there were photos included that had GPS data, they are viewable
+                on the map at the marker points. Mousing over the marker displays a
+                small preview of the image. Click on the marker to get a
+                slightly larger image displayed. Clicking on that image will take
+                you to a separate page with the photo further enlarged.
             </li>
             <li>Each page also includes trail descriptions,
                 links to related materials, and sample photos taken on the hike,
@@ -152,7 +175,7 @@ require "../php/global_boot.php";
     </div>
     <div id="caveat"> If you see something that you'd like
         to comment on - suggestions, improvements, things not working,
-        etc... <a href="mailto:krcowles29@gmail.com">email us!</a>
+        etc... <a href="mailto:admin@nmhikes.com">email us!</a>
     </div>
 </div>
 <div id="addon"></div>
