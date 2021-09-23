6220e2217555c916a9b00e7baf50df4dda178f65
Ken Cowles <krcowles29@gmail.com>
2021-09-23 13:12:53 -0600

    fix lastHiked when no photos are present

--- edit/lastHiked.php
+++ edit/lastHiked.php
@@ -12,7 +12,10 @@ $getPixReq
 $photoDates = $pdo->prepare($getPixReq);
 $photoDates->execute([$hikeNo]);
 $mostRecent = $photoDates->fetch(PDO::FETCH_ASSOC);
-$lastHiked = substr($mostRecent['date'], 0, 10); // dispose of 'time'
-$writeDateReq = "UPDATE `EHIKES` SET `last_hiked` = '{$lastHiked}' WHERE " .
-    "`indxNo` = {$hikeNo};";
-$writeDate = $pdo->query($writeDateReq);
+if ($mostRecent !== false && count($mostRecent) === 1) {
+    $lastHiked = substr($mostRecent['date'], 0, 10); // dispose of 'time'
+    $writeDateReq = "UPDATE `EHIKES` SET `last_hiked` = '{$lastHiked}' WHERE " .
+        "`indxNo` = {$hikeNo};";
+    $writeDate = $pdo->query($writeDateReq);
+}
+
